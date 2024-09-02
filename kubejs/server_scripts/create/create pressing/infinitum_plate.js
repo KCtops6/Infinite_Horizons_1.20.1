3315a79d2ec3dd27a59a0649152745da4232abf4
KCtops <126096649+KCtops@users.noreply.github.com>
2024-09-02 14:00:21 -0400

    moved andesite_gravel.js. moved diorite_gravel.js. moved dust.js. modified electron_tube.js. moved granite_gravel.js. moved infinitum_plate.js. moved limestone_gravel.js. modified replace_andesite_alloy.js. moved sand.js. moved tuff_gravel.js.

--- kubejs/server_scripts/create/create pressing/infinitum_plate.js
+++ kubejs/server_scripts/create/create pressing/infinitum_plate.js
@@ -1,5 +1,5 @@
 // https://wiki.latvian.dev/books/kubejs/page/kubejs-create
-// Syntax: pressing(output[], input)
+// Syntax: create pressing(output[], input)
 ServerEvents.recipes(e => {
     // ['kubejs:infinitium_ingot', 'kubejs:infinitium_plate']
     e.recipes.create.pressing('kubejs:infinitium_plate', 'kubejs:infinitium_ingot')
--- kubejs/server_scripts/create/replace_andesite_alloy.js
+++ kubejs/server_scripts/create/replace_andesite_alloy.js
@@ -1,5 +1,5 @@
 // https://wiki.latvian.dev/books/kubejs/page/kubejs-create
-// Syntax: pressing(output[], input)
+// Syntax: create pressing(output[], input)
 ServerEvents.recipes(event => {
     event.replaceInput(
         { input: 'create:andesite_alloy' }, 'create:andesite_alloy', 'kubejs:stone_alloy'
