65832a8e6c1ea58e26244f65b90ab06eab386565
KCtops <126096649+KCtops@users.noreply.github.com>
2024-09-02 16:00:13 -0400

    moved andesite_gravel.js. modified bookmarks.ini. moved diorite_gravel.js. moved dust.js. moved granite_gravel.js. created ground_meats_crushing.js.js. created ground_meats_milling.js.js. moved limestone_gravel.js. moved sand.js. moved stone_alloy.js. moved tuff_gravel.js.

---
+++ kubejs/server_scripts/create/create crushing/pam's harvestcraft/ground_meats_crushing.js
@@ -0,0 +1,7 @@
+ServerEvents.recipes(event => {
+    const meats = ['rabbit', 'mutton', 'pork', 'beef', 'chicken'];
+    meats.forEach(meat => {
+        event.recipes.createCrushing([`pamhc2foodcore:ground${meat}item`], `#forge:raw${meat}`);
+    });
+    event.recipes.createCrushing([`pamhc2foodcore:groundfishitem`], `#forge:fishes`);
+});
---
+++ kubejs/server_scripts/create/create milling/pam's harvestcraft/ground_meats_milling.js
@@ -0,0 +1,7 @@
+ServerEvents.recipes(event => {
+    const meats = ['rabbit', 'mutton', 'pork', 'beef', 'chicken'];
+    meats.forEach(meat => {
+        event.recipes.createMilling([`pamhc2foodcore:ground${meat}item`], `#forge:raw${meat}`);
+    });
+    event.recipes.createMilling([`pamhc2foodcore:groundfishitem`], `#forge:fishes`);
+});
