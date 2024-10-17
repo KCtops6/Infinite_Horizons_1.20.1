81cfc9e2fd71a52e098b33fea11432e7cbe23988
KCtops <126096649+KCtops@users.noreply.github.com>
2024-10-17 11:43:13 -0400

    cleaned up create pressing server script.

--- kubejs/server_scripts/create/create pressing/infinitum_plate.js
+++ kubejs/server_scripts/create/create pressing/infinitum_plate.js
@@ -1,5 +1,4 @@
-// https://wiki.latvian.dev/books/kubejs/page/kubejs-create
-// Syntax: create pressing(output[], input)
+// https://kubejs.com/wiki/addons/create#pressing
 ServerEvents.recipes(e => {
     // ['kubejs:infinitium_ingot', 'kubejs:infinitium_plate']
     e.recipes.create.pressing('kubejs:infinitium_plate', 'kubejs:infinitium_ingot')
