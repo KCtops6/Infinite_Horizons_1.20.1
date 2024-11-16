39a2126633fd9fa7b4597ca70736005eaf2b5d37
KCtops100 <kylecollamer@gmail.com>
2024-11-16 17:06:36 -0500

    Added Create Mixing recipe for Powah dielectric paste.

---
+++ kubejs/server_scripts/create/create mixing/dielectric_paste.js
@@ -0,0 +1,8 @@
+// https://kubejs.com/wiki/addons/create#mixing
+ServerEvents.recipes(event => {
+    event.recipes.createMixing('24x powah:dielectric_paste', [
+        '3x #minecraft:coals',
+        '2x minecraft:clay_ball',
+        Fluid.lava(1000)
+    ]).heated()
+})
