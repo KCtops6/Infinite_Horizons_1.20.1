60c5898e88f31d513feab35e7b5a1add11dc9ce4
KCtops100 <kylecollamer@gmail.com>
2024-11-16 13:40:22 -0500

    Added Create Mixing Recipes for Bronze, Constantan, Electrum, Invar Ingots.

--- kubejs/server_scripts/create/create mixing/tin_alloy_ingot.js
+++
@@ -1,7 +0,0 @@
-// https://kubejs.com/wiki/addons/create#mixing
-ServerEvents.recipes(event => {
-    event.recipes.createMixing('gtceu:tin_alloy_ingot', [
-        '#forge:ingots/iron',
-        '#forge:ingots/tin'
-    ]).superheated()
-})
