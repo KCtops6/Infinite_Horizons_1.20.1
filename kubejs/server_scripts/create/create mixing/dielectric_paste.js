38a5fbdc824af8729d24a738551003718db93ce3
KCtops100 <kylecollamer@gmail.com>
2024-11-21 12:52:25 -0500

    Made the Create Mixing Dielectric Paste recipe better.

--- kubejs/server_scripts/create/create mixing/dielectric_paste.js
+++ kubejs/server_scripts/create/create mixing/dielectric_paste.js
@@ -1,6 +1,6 @@
 // https://kubejs.com/wiki/addons/create#mixing
 ServerEvents.recipes(event => {
-    event.recipes.createMixing('24x powah:dielectric_paste', [
+    event.recipes.createMixing('48x powah:dielectric_paste', [
         '3x #minecraft:coals',
         '2x minecraft:clay_ball',
         Fluid.lava(1000)
