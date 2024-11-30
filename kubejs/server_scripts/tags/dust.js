e331681d7634017ea279cd9c066b27af726df0a1
KCtops100 <kylecollamer@gmail.com>
2024-11-29 20:40:38 -0500

    Added KubeJS obsidian dust.

--- kubejs/server_scripts/tags/dust.js
+++ kubejs/server_scripts/tags/dust.js
@@ -1,3 +1,4 @@
 ServerEvents.tags('item', event => {
+    event.add('forge:dusts/obsidian', 'kubejs:obsidian_dust');
     event.add('forge:dusts/sulfur', 'kubejs:sulfur_dust');
 });
--- kubejs/startup_scripts/ore dictionary/dust.js
+++ kubejs/startup_scripts/ore dictionary/dust.js
@@ -1,3 +1,4 @@
 StartupEvents.registry('item', e => {
+    e.create('obsidian_dust').displayName('Obsidian Dust');
     e.create('sulfur_dust').displayName('Sulfur Dust');
 });
