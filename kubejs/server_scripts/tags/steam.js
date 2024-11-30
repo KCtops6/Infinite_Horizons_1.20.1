8383ea5edc52dd31407f2c47058f7836adbc7a59
KCtops100 <kylecollamer@gmail.com>
2024-11-30 12:12:34 -0500

    Modified Gregtech questlines.

--- kubejs/server_scripts/tags/steam.js
+++ kubejs/server_scripts/tags/steam.js
@@ -1,19 +1,17 @@
-// ServerEvents.tags('item', event => {
-//     event.add('forge:steam', 'advgenerators:steam');    // Adds the steam from Advanced Generators mod into my newly created steam tag.
-//     event.add('forge:steam', 'gtceu:steam');    // Adds the steam from Gregtech mod into my newly created steam tag.
-//     event.add('forge:steam', 'mekanism:steam'); // Adds the steam from Mekanism mod into my newly created steam tag.
-//     event.add('forge:steam', 'kubejs:steam');   // Adding my own steam from KubeJS into my newly created steam tag.
-// })
-
 // ServerEvents.recipes(event => {
-//     event.replaceInput(
-//         { input: '#forge:steam' },  // All recipes that have input of any steam
-//         '#forge:steam', // Replacing every steam...
-//         'kubejs:steam'  // ...with KubeJS steam.
-//     )
-//     event.replaceOutput(
-//         { output: '#forge:steam' }, // All recipes that have output of any steam
-//         '#forge:steam', // Replacing every steam...
-//         'kubejs:steam'  // ...with KubeJS steam.
-//     )
+//     function replaceSteam(fluid) {
+//         event.replaceInput(
+//             { input: Fluid.of(fluid) },  // All recipes that have input of any steam
+//             Fluid.of(fluid), // Replacing every steam...
+//             'kubejs:steam'  // ...with KubeJS steam.
+//         )
+//         event.replaceOutput(
+//             { output: Fluid.of(fluid) }, // All recipes that have output of any steam
+//             Fluid.of(fluid), // Replacing every steam...
+//             'kubejs:steam'  // ...with KubeJS steam.
+//         )
+//     }
+//     replaceSteam('advgenerators:steam');
+//     replaceSteam('gtceu:steam');
+//     replaceSteam('mekanism:steam')
 // })
