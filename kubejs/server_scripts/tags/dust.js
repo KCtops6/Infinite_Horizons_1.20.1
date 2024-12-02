3e60385a69e54f0a2c6ff72087fff748c291e973
KCtops100 <kylecollamer@gmail.com>
2024-12-01 21:48:02 -0500

    Added KubeJS coal dust. Added KubeJS charcoal dust.

--- kubejs/server_scripts/tags/dust.js
+++ kubejs/server_scripts/tags/dust.js
@@ -1,4 +1,6 @@
 ServerEvents.tags('item', event => {
+    event.add('forge:dusts/charcoal', 'kubejs:charcoal_dust');
+    event.add('forge:dusts/coal', 'kubejs:coal_dust');
     event.add('forge:dusts/obsidian', 'kubejs:obsidian_dust');
     event.add('forge:dusts/sulfur', 'kubejs:sulfur_dust');
 });
--- kubejs/startup_scripts/ore dictionary/dust.js
+++ kubejs/startup_scripts/ore dictionary/dust.js
@@ -1,4 +1,6 @@
 StartupEvents.registry('item', e => {
+    e.create('charcoal_dust').displayName('Charcoal Dust');
+    e.create('coal_dust').displayName('Coal Dust');
     e.create('obsidian_dust').displayName('Obsidian Dust');
     e.create('sulfur_dust').displayName('Sulfur Dust');
 });
