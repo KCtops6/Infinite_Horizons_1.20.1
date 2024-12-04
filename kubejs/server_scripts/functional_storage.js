d4241281740fbb85c8ab6405e9a770fc071da42a
KCtops100 <kylecollamer@gmail.com>
2024-12-04 12:27:14 -0500

    Added custom compacting recipe for gregtech gallium arsenide dust.

--- kubejs/server_scripts/functional_storage.js
+++ kubejs/server_scripts/functional_storage.js
@@ -1,5 +1,16 @@
 ServerEvents.recipes(event => {
-    event.remove({id: 'functionalstorage:oak_drawer_alternate_x1'});
-    event.remove({id: 'functionalstorage:oak_drawer_alternate_x2'});
-    event.remove({id: 'functionalstorage:oak_drawer_alternate_x4'});
+    event.remove({id: 'functionalstorage:oak_drawer_alternate_x1'})
+    event.remove({id: 'functionalstorage:oak_drawer_alternate_x2'})
+    event.remove({id: 'functionalstorage:oak_drawer_alternate_x4'})
+    event.custom({ 
+        type: "functionalstorage:custom_compacting", 
+        higher_input: { 
+          count: 1, 
+          item: "gtceu:gallium_arsenide_dust"
+        }, 
+        lower_input: { 
+          count: 4, 
+          item: "gtceu:small_gallium_arsenide_dust"
+        } 
+    })
 })
