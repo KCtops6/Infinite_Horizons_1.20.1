d686c48a1489969c69f935a8f4d5f07c3d54db1f
KCtops100 <kylecollamer@gmail.com>
2024-12-05 15:49:07 -0500

    refactored functional storage server script

--- kubejs/server_scripts/functional_storage.js
+++ kubejs/server_scripts/functional_storage.js
@@ -13,4 +13,15 @@ ServerEvents.recipes(event => {
           item: "gtceu:small_gallium_arsenide_dust"
         } 
     })
+    event.custom({ 
+      type: "functionalstorage:custom_compacting", 
+      higher_input: { 
+        count: 1, 
+        item: "gtceu:ash_dust"
+      }, 
+      lower_input: { 
+        count: 9, 
+        item: "gtceu:tiny_ash_dust"
+      } 
+    })
 })
