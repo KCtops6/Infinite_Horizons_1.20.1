2da2a67a358116c96a1175a28eb47f1455246004
KCtops100 <kylecollamer@gmail.com>
2024-12-07 10:42:39 -0500

    reformatted rescources startup script. changed brass block texture changed brass ingot texture

--- kubejs/startup_scripts/block,ingot,plate,dust.js
+++ kubejs/startup_scripts/block,ingot,plate,dust.js
@@ -1,58 +1,54 @@
 StartupEvents.registry('block', event => {
+    const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
+
     const blocks = [
-        'brass', 'bronze', 'constantan', 'copper', 'electrum', 'gold', 'infinitium', 'invar',
-        'iron', 'lead', 'nickel', 'osmium', 'silver', 'tin', 'zinc'
+        'brass', 'bronze', 'constantan', 'copper', 'electrum', 'gold',
+        'infinitium', 'invar', 'iron', 'lead', 'nickel', 'osmium', 'silver', 'tin', 'zinc'
     ];
-    
+
     blocks.forEach(block => {
         event.create(`${block}_block`)
-        .displayName(`${capitalize(block)} Block`)
-        .soundType('metal')
-        .hardness(5.0)
-        .resistance(6.0)
-        .requiresTool(true)
-        .tagBlock('minecraft:mineable/pickaxe')
-        .tagBlock('minecraft:needs_iron_tool')
+            .displayName(`${capitalize(block)} Block`)
+            .soundType('metal')
+            .hardness(5.0)
+            .resistance(6.0)
+            .requiresTool(true)
+            .tagBlock('minecraft:mineable/pickaxe')
+            .tagBlock('minecraft:needs_iron_tool');
     });
+});
+
+StartupEvents.registry('item', event => {
+    const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
 
     const dusts = [
-        'brass', 'bronze', 'charcoal', 'constantan',
-        'coal', 'copper', 'electrum', 'gold', 'invar',
-        'iron', 'lead', 'nickel', 'obsidian', 'osmium',
-        'sapphire', 'silver', 'steel', 'sulfur', 'tin', 'zinc'
+        'brass', 'bronze', 'charcoal', 'constantan', 'coal', 'copper',
+        'electrum', 'gold', 'invar', 'iron', 'lead', 'nickel', 'obsidian',
+        'osmium', 'sapphire', 'silver', 'steel', 'sulfur', 'tin', 'zinc'
     ];
 
     dusts.forEach(dust => {
-        e.create(`${dust}_dust`).displayName(`${capitalize(dust)} Dust`);
+        event.create(`${dust}_dust`).displayName(`${capitalize(dust)} Dust`);
     });
 
     const ingots = [
-        'brass', 'bronze', 'constantan',
-        'electrum', 'infinitium', 'invar',
-        'lead', 'nickel', 'osmium',
-        'silver', 'steel', 'tin', 'zinc'
+        'brass', 'bronze', 'constantan', 'electrum', 'infinitium', 'invar',
+        'lead', 'nickel', 'osmium', 'silver', 'steel', 'tin', 'zinc'
     ];
 
     ingots.forEach(ingot => {
-        e.create(`${ingot}_ingot`).displayName(`${capitalize(ingot)} Ingot`);
+        event.create(`${ingot}_ingot`).displayName(`${capitalize(ingot)} Ingot`);
     });
 
     const plates = [
-        'brass', 'bronze', 'constantan',
-        'copper', 'electrum', 'gold',
-        'infinitium', 'invar', 'iron',
-        'lead', 'netherite', 'nickel',
+        'brass', 'bronze', 'constantan', 'copper', 'electrum', 'gold',
+        'infinitium', 'invar', 'iron', 'lead', 'netherite', 'nickel',
         'silver', 'tin', 'zinc'
     ];
 
     plates.forEach(plate => {
-        e.create(`${plate}_plate`).displayName(`${capitalize(plate)} Plate`);
+        event.create(`${plate}_plate`).displayName(`${capitalize(plate)} Plate`);
     });
 
-    e.create('silicon').displayName('Silicon');
-    
-})
-
-function capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-}
+    event.create('silicon').displayName('Silicon');
+});
