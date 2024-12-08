69972a0eeebd37dfa4565562b2ec8f8cd1a49c81
KCtops100 <kylecollamer@gmail.com>
2024-12-08 11:35:40 -0500

    Removed observe soul fire quest. fixed foliot crushing recipes.

--- kubejs/server_scripts/occultism.js
+++ kubejs/server_scripts/occultism.js
@@ -1,3 +1,18 @@
 ServerEvents.recipes(event => {
-    event.replaceOutput({ id: 'minecraft:stick' }, 'minecraft:stick', '#minecraft:saplings')
-})
+    const replacements = [
+        { id: 'occultism:crushing/end_stone_dust', output: 'occultism:crushed_end_stone' },
+        { id: 'occultism:crushing/iesnium_dust_from_ingot', output: 'occultism:iesnium_dust' },
+        { id: 'occultism:crushing/iesnium_dust', output: '2x occultism:iesnium_dust' },
+        { id: 'occultism:crushing/iesnium_dust_from_raw', output: '2x occultism:iesnium_dust' },
+        { id: 'occultism:crushing/iesnium_dust_from_raw_block', output: '18x occultism:iesnium_dust' },
+        { id: 'occultism:crushing/arcane_crystal_dust', output: '4x forbidden_arcanus:arcane_crystal_dust' },
+        { id: 'occultism:crushing/arcane_crystal_dust_from_gem', output: 'forbidden_arcanus:arcane_crystal_dust' },
+        { id: 'occultism:crushing/redstone_dust', output: '4x minecraft:redstone_dust' },
+        { id: 'occultism:crushing/blaze_powder_from_rod', output: 'minecraft:blaze_powder' },
+        { id: 'occultism:crushing/topaz_dust_from_gem', output: 'gtceu:topaz_dust' }
+    ];
+
+    replacements.forEach(replacement => {
+        event.replaceOutput({ id: replacement.id }, 'minecraft:barrier', replacement.output);
+    });
+});
