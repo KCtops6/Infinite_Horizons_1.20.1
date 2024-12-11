1729f5d42c4fc20bbcd662215d6f1b53839626c6
KCtops100 <kylecollamer@gmail.com>
2024-12-11 11:51:51 -0500

    added recipe for void cake.

--- kubejs/server_scripts/crafting_components.js
+++ kubejs/server_scripts/crafting_components.js
@@ -1,3 +1,4 @@
 ServerEvents.recipes(event => {
     event.recipes.createCompacting('kubejs:void_cake_base', ['minecraft:egg', 'minecraft:sugar', 'kubejs:void_flour']);
+    event.recipes.createFilling('kubejs:void_cake', ['kubejs:void_cake_base', Fluid.of('kubejs:liquid_ender_pearl')]);
 })
