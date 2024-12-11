73226c33bd247622aece2ed956c153cb04b48fe1
KCtops100 <kylecollamer@gmail.com>
2024-12-11 13:17:54 -0500

    reformatted items startup script

--- kubejs/server_scripts/crafting_components.js
+++ kubejs/server_scripts/crafting_components.js
@@ -20,37 +20,7 @@ ServerEvents.recipes(event => {
     );
 
     // Nether Painting Crafting Components
-    // Infernal Canvas
-    event.recipes.extendedcrafting.shapeless_table(
-        'infernal_canvas',
-        [
-            'farmersdelight:canvas',
-            'minecraft:blaze_powder',
-            'minecraft:nether_wart'
-        ]
-    ).tier(1);
-    // Obsidian Frame
-    event.recipes.extendedcrafting.shaped_table(
-        'kubejs:obsidian_frame',
-        [
-            'abbba',
-            'b   b',
-            'b   b',
-            'b   b',
-            'abbba'
-        ],
-        {a: 'mysticalagriculture:supremium_gemstone'},
-        {b: 'forbiddenarcanus:obsidian_ingot'}
-    ).tier(2);
-    // Soul Ink
-    event.recipes.extendedcrafting.shapeless_table(
-        'kubejs:soul_ink',
-        [
-            'quark:soul_bead',
-            'minecraft:ink_sac',
-            'mysticalagriculture:soulium_dust'
-        ]
-    ).tier(1);
+
     // Infernal Reactor Core
     event.recipes.extendedcrafting.shaped_table(
         'kubejs:infernal_reactor_core',
@@ -74,19 +44,22 @@ ServerEvents.recipes(event => {
     // Nether Painting
     event.recipes.extendedcrafting.combination(
         'dimpaintings:nether_painting',
-        'kubejs:nether_painting_base',
+        'minecraft:painting',
         [
             'kubejs:red_candle',
             'kubejs:infernal_reactor_core',
         ], 
-        10000000
+        10000
     );
 
     // End Painting Crafting Components
+
     // Void Cake Base
     event.recipes.createCompacting('kubejs:void_cake_base', ['minecraft:egg', 'minecraft:sugar', 'kubejs:void_flour']);
+
     // Void Cake
     event.recipes.createFilling('kubejs:void_cake', ['kubejs:void_cake_base', Fluid.of('kubejs:liquid_ender_pearl')]);
+
     // Dimensional Phase Shifter
     event.recipes.extendedcrafting.shaped_table(
         'dimensional_phase_shifter',
@@ -106,16 +79,35 @@ ServerEvents.recipes(event => {
         {e: 'mekanism:hdpe_elytra'},
         {f: 'mekanism:laser'}
     ).tier(3);
-
     
+    // Astral Chorus Gem
+    event.recipes.extendedcrafting.shaped_table(
+        'dimensional_phase_shifter',
+        [
+            'abbbbba',
+            'b     b',
+            'b  d  b',
+            'b fcf b',
+            'b  e  b',
+            'b     b',
+            'abbbbba'
+        ],
+        {a: 'mekanism:ultimate_control_circuit'},
+        {b: 'mekanism:alloy_atomic'},
+        {c: 'mekanism:quantum_entangloporter'},
+        {d: 'mekanism:industrial_alarm'},
+        {e: 'mekanism:hdpe_elytra'},
+        {f: 'mekanism:laser'}
+    ).tier(3);
+
     // End Painting
     event.recipes.extendedcrafting.combination(
         'dimpaintings:end_painting',
-        'kubejs:end_painting_base',
+        'minecraft:painting',
         [
             'kubejs:void_cake',
-            'dimensional_phase_shifter',
+            'kubejs:dimensional_phase_shifter',
         ], 
-        10000000
+        10000
     );
 })
--- kubejs/startup_scripts/items.js
+++ kubejs/startup_scripts/items.js
@@ -1,54 +1,66 @@
+// StartupEvents.registry('item', e => {
+//     e.create('arrow').displayName('Arrow');
+//     e.create('blue_candle').displayName('Blue Candle');
+//     e.create('bomb').displayName('Bomb');
+//     e.create('boomerang').displayName('Boomerang');
+//     e.create('bow').displayName('Bow');
+//     e.create('chicken_fried_steak_ingot').displayName('Chicken Fried Steak Ingot');
+//     e.create("dark_matter").displayName("Dark Matter");
+//     e.create("weak_echo_shard").displayName("Weak Echo Shard");
+//     e.create("mycellium_spores").displayName("Mycellium Spores");
+//     e.create("red_mushroom_spores").displayName("Red Mushroom Spores");
+//     e.create("brown_mushroom_spores").displayName("Brown Mushroom Spores");
+//     e.create("crimson_nylium_spores").displayName("Crimson Nylium Spores");
+//     e.create("warped_nylium_spores").displayName("Warped Nylium Spores");
+//     e.create('desert_temple_dungeon_key').displayName('Desert Temple Dungeon Key');
+//     e.create('explorer_map').displayName('Explorer\'s Map');
+//     e.create('forbidden_transmutation_matter').displayName('Forbidden Transmutation Matter');
+//     e.create('jungle_temple_dungeon_key').displayName('Jungle Temple Dungeon Key');
+//     e.create('magical_boomerang').displayName('Magical Boomerang');
+//     e.create('magical_shield').displayName('Magical Shield');
+//     e.create('magical_sword').displayName('Magical Sword');
+//     e.create('mekanism_mystery_box').displayName('Mekanism Mystery Box');
+//     e.create('netherite_wire').displayName('Netherite Wire');
+//     e.create('red_candle', 'sword').displayName('Red Candle').tier('netherite').attackDamageBaseline(16.0);
+//     e.create('silver_arrow').displayName('Silver Arrow');
+//     e.create('stone_alloy').displayName('Stone Alloy');
+//     e.create('stronghold_dungeon_key').displayName('Stronghold Dungeon Key');
+//     e.create('sword').displayName('Sword');
+//     e.create('white_sword').displayName('White Sword');
+//     e.create('wooden_shield').displayName('Wooden Shield');
+
+//     e.create('void_flour').displayName('Void Flour');
+//     e.create('void_cake_base').displayName('Void Cake Base');
+//     e.create('void_cake').displayName('Void Cake');
+
+//     e.create('nether_painting_base').displayName('Nether Painting Base');
+//     e.create('end_painting_base').displayName('End Painting Base');
+
+//     e.create('infernal_canvas').displayName('Infernal Canvas');
+//     e.create('obsidian_frame').displayName('Obsidian Frame');
+
+//     e.create('infernal_reactor_core').displayName('Infernal Reactor Core');
+//     e.create('dimensional_phase_shifter').displayName('Dimensional Phase Shifter');
+
+//     e.create('rune_of_inferno').displayName('Rune of Inferno');
+
+// })
+
 StartupEvents.registry('item', e => {
-    e.create('arrow').displayName('Arrow');
-    e.create('blue_candle').displayName('Blue Candle');
-    e.create('bomb').displayName('Bomb');
-    e.create('boomerang').displayName('Boomerang');
-    e.create('bow').displayName('Bow');
-    e.create('chicken_fried_steak_ingot').displayName('Chicken Fried Steak Ingot');
-    /**
-     * DARK MATTER
-     */
-    e.create("dark_matter").displayName("Dark Matter");
-    /**
-     * WEAK ECHO SHARD
-     */
-    e.create("weak_echo_shard").displayName("Weak Echo Shard");
-    /**
-     * MUSHROOM SPORES
-     */
-    e.create("mycellium_spores").displayName("Mycellium Spores");
-    e.create("red_mushroom_spores").displayName("Red Mushroom Spores");
-    e.create("brown_mushroom_spores").displayName("Brown Mushroom Spores");
-    e.create("crimson_nylium_spores").displayName("Crimson Nylium Spores");
-    e.create("warped_nylium_spores").displayName("Warped Nylium Spores");
-    e.create('desert_temple_dungeon_key').displayName('Desert Temple Dungeon Key');
-    e.create('explorer_map').displayName('Explorer\'s Map');
-    e.create('forbidden_transmutation_matter').displayName('Forbidden Transmutation Matter');
-    e.create('jungle_temple_dungeon_key').displayName('Jungle Temple Dungeon Key');
-    e.create('magical_boomerang').displayName('Magical Boomerang');
-    e.create('magical_shield').displayName('Magical Shield');
-    e.create('magical_sword').displayName('Magical Sword');
-    e.create('mekanism_mystery_box').displayName('Mekanism Mystery Box');
-    e.create('netherite_wire').displayName('Netherite Wire');
-    e.create('red_candle', 'sword').displayName('Red Candle').tier('netherite').attackDamageBaseline(16.0);
-    e.create('silver_arrow').displayName('Silver Arrow');
-    e.create('stone_alloy').displayName('Stone Alloy');
-    e.create('stronghold_dungeon_key').displayName('Stronghold Dungeon Key');
-    e.create('sword').displayName('Sword');
-    e.create('white_sword').displayName('White Sword');
-    e.create('wooden_shield').displayName('Wooden Shield');
+    const items = [
+        'arrow', 'blue_candle', 'bomb', 'boomerang', 'bow', 'chicken_fried_steak_ingot', 'dark_matter', 'weak_echo_shard', 'mycellium_spores',
+        'red_mushroom_spores', 'brown_mushroom_spores', 'crimson_nylium_spores', 'warped_nylium_spores', 'desert_temple_dungeon_key', 'explorer_map',
+        'forbidden_transmutation_matter', 'jungle_temple_dungeon_key', 'magical_boomerang', 'magical_shield', 'magical_sword', 'mekanism_mystery_box',
+        'netherite_wire', 'silver_arrow', 'stone_alloy', 'stronghold_dungeon_key', 'sword', 'white_sword', 'wooden_shield', 'void_flour', 'void_cake_base',
+        'void_cake', 'nether_painting_base', 'end_painting_base', 'infernal_canvas', 'obsidian_frame', 'infernal_reactor_core', 'dimensional_phase_shifter',
+        'rune_of_inferno'
+    ];
 
-    e.create('void_flour').displayName('Void Flour');
-    e.create('void_cake_base').displayName('Void Cake Base');
-    e.create('void_cake').displayName('Void Cake');
+    const formatName = (id) => {
+        return id.split('_').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+    };
 
-    e.create('nether_painting_base').displayName('Nether Painting Base');
-    e.create('end_painting_base').displayName('End Painting Base');
-
-    e.create('infernal_canvas').displayName('Infernal Canvas');
-    e.create('obsidian_frame').displayName('Obsidian Frame');
-
-    e.create('infernal_reactor_core').displayName('Infernal Reactor Core');
-    e.create('dimensional_phase_shifter').displayName('Dimensional Phase Shifter');
-
-})
+    items.forEach(id => {
+        e.create(id).displayName(formatName(id));
+    });
+});
