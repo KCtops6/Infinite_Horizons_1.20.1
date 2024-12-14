52d9ddae9a303dad7e0561c6edff1be660a070c9
KCtops100 <kylecollamer@gmail.com>
2024-12-14 10:55:55 -0500

    reformatted startup scripts updated Sophisticated Storage.

--- kubejs/server_scripts/extended_crafting.js
+++
@@ -1,3 +0,0 @@
-ServerEvents.recipes(event => {
-    event.recipes.extendedcrafting.combination("minecraft:chest", '#forge:rods/wooden', ['#minecraft:logs', '#minecraft:logs','#minecraft:logs','#minecraft:logs','#minecraft:logs','#minecraft:logs','#minecraft:logs','#minecraft:logs']).powerRate(10000);
-})
