681f615978ff308b368273cfce7cc5da91dad5a1
KCtops100 <kylecollamer@gmail.com>
2024-12-15 17:25:41 -0500

    removed extended crafting server scripts

--- kubejs/server_scripts/extended_crafting.js
+++
@@ -1,9 +0,0 @@
-ServerEvents.recipes(event => {
-    event.recipes.extendedcrafting.combination(
-        'kubejs:lava_infuser',
-        'mekanism:metallurgic_infuser',
-        [
-            'thermal:charge_bench'
-        ]
-    ).powerRate(1000);
-});
