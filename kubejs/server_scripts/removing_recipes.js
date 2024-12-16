1855b65f7fb0fa6a912e08e4c4685e5a94b262ba
KCtops100 <kylecollamer@gmail.com>
2024-12-16 11:18:16 -0500

    reformatted removing recipes server script

--- kubejs/server_scripts/removing_recipes.js
+++ kubejs/server_scripts/removing_recipes.js
@@ -1,8 +1,18 @@
 // https://kubejs.com/wiki/tutorials/recipes#removing-recipes
 ServerEvents.recipes(event => {
-    /**
-     * MEKANISM
-     */
-    event.remove({ output: 'mekanism:digital_miner' })
-    event.remove({mod: 'dimpaintings'})
-})
+    // removing by output
+    const ids = [];
+    ids.forEach(id => {
+        event.remove({ id: id });
+    });
+    // removing by output
+    const outputs = ['mekanism:digital_miner'];
+    outputs.forEach(output => {
+        event.remove({ output: output });
+    });
+    // removing by mod
+    const mods = ['dimpaintings'];
+    mods.forEach(mod => {
+        event.remove({mod: mod});
+    });
+});
