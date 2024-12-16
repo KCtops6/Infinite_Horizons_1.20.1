22a64d560cb5011e533f1ba157fad1cccfa81962
KCtops100 <kylecollamer@gmail.com>
2024-12-16 14:55:11 -0500

    added overworld painting recipe

---
+++ kubejs/server_scripts/extended_crafting.js
@@ -0,0 +1,55 @@
+ServerEvents.recipes(event => {
+    // dimensional paintings
+    const dimensionalPaintings = [
+        {
+            power_cost: 100000,
+            ingredient1: 'mysticalagriculture:air_essence',
+            ingredient2: 'mysticalagriculture:earth_essence',
+            ingredient3: 'mysticalagriculture:water_essence',
+            ingredient4: 'mysticalagriculture:fire_essence',
+            ingredient5: 'mysticalagriculture:dirt_essence',
+            ingredient6: 'mysticalagriculture:stone_essence',
+            ingredient7: 'mysticalagriculture:wood_essence',
+            ingredient8: 'mysticalagriculture:nature_essence',
+            result: 'dimpaintings:overworld_painting'
+        }
+    ]
+    dimensionalPaintings.forEach(painting => {
+        event.custom({
+            type: "extendedcrafting:combination",
+            power_cost: painting.power_cost,
+            input: {
+                item: "minecraft:painting"
+            },
+            ingredients: [
+                {
+                    item: painting.ingredient1
+                },
+                {
+                    item: painting.ingredient2
+                },
+                {
+                    item: painting.ingredient3
+                },
+                {
+                    item: painting.ingredient4
+                },
+                {
+                    item: painting.ingredient5
+                },
+                {
+                    item: painting.ingredient6
+                },
+                {
+                    item: painting.ingredient7
+                },
+                {
+                    item: painting.ingredient8
+                }
+            ],
+            result: {
+                item: painting.result
+            }
+        });
+    });
+});
