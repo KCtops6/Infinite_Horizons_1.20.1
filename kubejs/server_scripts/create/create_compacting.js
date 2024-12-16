24d6d6b5db892d663801c2d150207b5220d6623b
KCtops100 <kylecollamer@gmail.com>
2024-12-16 14:41:04 -0500

    fixed sculk recipe.

--- kubejs/server_scripts/create/create_compacting.js
+++ kubejs/server_scripts/create/create_compacting.js
@@ -19,4 +19,21 @@ ServerEvents.recipes(event => {
             }
         ]
     });
+
+    // void cake base
+    event.custom({
+        type: "create:compacting",
+        heatRequirement: "superheated",
+        ingredients: [
+            {
+                count: 4,
+                item: 'kubejs:dark_matter'
+            }
+        ],
+        results: [
+            {
+                item: 'minecraft:sculk'
+            }
+        ]
+    });
 });
