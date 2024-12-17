9bcaaae31991b59711fe92b24b3e9362cf59c60b
KCtops100 <kylecollamer@gmail.com>
2024-12-17 12:54:31 -0500

    changed recipe for extended crafting flux crafter (plus added quest). changed recipe for extended crafting flux alternator (plus added quest).

--- kubejs/server_scripts/extended_crafting.js
+++ kubejs/server_scripts/extended_crafting.js
@@ -126,4 +126,54 @@ ServerEvents.recipes(event => {
             item: "extendedcrafting:ender_alternator"
         }
     });
+
+    // flux crafter
+    event.custom({
+        type: "extendedcrafting:shaped_table",
+        pattern: [
+            "IIIII",
+            "RCCCR",
+            "RCCCR",
+            "RCCCR",
+            "RRRRR"
+        ],
+        key: {
+            C: {
+                item: "create:mechanical_crafter"
+            },
+            R: {
+                item: "extendedcrafting:redstone_ingot"
+            },
+            I: {
+                item: "kubejs:infinitium_plate"
+            }
+        },
+        result: {
+            item: "extendedcrafting:flux_crafter"
+        }
+    });
+
+    // flux alternator
+    event.custom({
+        type: "extendedcrafting:shaped_table",
+        pattern: [
+            "INI",
+            "IEI",
+            "EEE"
+        ],
+        key: {
+            N: {
+                item: "minecraft:nether_star"
+            },
+            E: {
+                item: "extendedcrafting:redstone_ingot"
+            },
+            I: {
+                item: "kubejs:infinitium_plate"
+            }
+        },
+        result: {
+            item: "extendedcrafting:flux_alternator"
+        }
+    });
 });
