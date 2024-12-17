9489efc8aa6de8a12088e89ee90a413595657ce6
KCtops100 <kylecollamer@gmail.com>
2024-12-17 16:39:40 -0500

    added ars nouveau crushing recipes for gravels.

---
+++ kubejs/server_scripts/ars nouveau/crush.js
@@ -0,0 +1,28 @@
+ServerEvents.recipes(event => {
+    // simple crushing input to output mappings (no chanced outputs)
+    const recipes = [
+        { input: 'minecraft:sand', output: 'createsifter:dust'},
+        { input: 'minecraft:gravel', output: 'createsifter:sand'},
+        { input: 'minecraft:netherrack', output: 'kubejs:netherrack_gravel'},
+        { input: 'minecraft:basalt', output: 'kubejs:basalt_gravel'},
+        { input: 'minecraft:end_stone', output: 'kubejs:end_stone_gravel'}
+    ]
+    recipes.forEach(recipe => {
+        event.custom({
+            type: "ars_nouveau:crush",
+            input: {
+                item: recipe.input
+            },
+            output: [
+                {
+                    chance: 1.0,
+                    maxRange: 1,
+                    stack: {
+                        count: 1,
+                        id: recipe.output
+                    }
+                }
+            ]
+        });
+    });
+});
