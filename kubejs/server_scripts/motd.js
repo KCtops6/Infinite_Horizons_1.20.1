fa2430166e7ecb0ba9e0f33be391d10bad16e091
KCtops100 <kylecollamer@gmail.com>
2024-12-17 19:20:00 -0500

    updated motd

--- kubejs/server_scripts/motd.js
+++ kubejs/server_scripts/motd.js
@@ -1,4 +1,31 @@
 PlayerEvents.loggedIn(event => {
-    event.player.tell(['Welcome to ', Text.blue('Monifactory Beta'), ` on ${capitalize(`${global.packmode}`)} mode.`]); // force recast to string
-    event.player.tell(['Report pack issues to ', Text.blue('the Github').underlined().clickOpenUrl('https://github.com/ThePansmith/Monifactory').hover('Click to open'), '.']);
-})
+    let player = event.player;
+
+    // Welcome message
+    player.tell(Text.of("§6§lWelcome to §aInfinite Horizons§6!"));
+
+    // Clickable links
+    player.tell(
+        Text.of("§eCheck out the project on CurseForge ").append(
+            Text.of("§b§nhere")
+                .click("open_url", "https://www.curseforge.com/projects/infinite-horizons")
+                .hover("show_text", "§aClick to visit the CurseForge page!")
+        )
+    );
+
+    player.tell(
+        Text.of("§eView the source code and issues on GitHub ").append(
+            Text.of("§b§nhere")
+                .click("open_url", "https://github.com/username/infinite-horizons")
+                .hover("show_text", "§aClick to visit the GitHub repo!")
+        )
+    );
+
+    player.tell(
+        Text.of("§eJoin our community on Discord ").append(
+            Text.of("§b§nhere")
+                .click("open_url", "https://discord.gg/yourdiscordlink")
+                .hover("show_text", "§aClick to join the Discord server!")
+        )
+    );
+});
