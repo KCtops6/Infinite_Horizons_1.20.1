9a4bc2e3605634230151d981a9821575d7bec5bc
KCtops100 <kylecollamer@gmail.com>
2024-12-20 11:20:01 -0500

    added recipes for late game natures aura items

--- kubejs/server_scripts/natural altar infusion/tained_gold_block.js
+++
@@ -1,5 +0,0 @@
-ServerEvents.recipes((event) => {
-    // ['naturesaura:tainted_gold_block', 'minecraft:gold_block']
-    event.remove({input: 'minecraft:gold_block', output: 'naturesaura:tainted_gold_block' })
-    event.recipes.naturesaura.altar('naturesaura:tainted_gold_block', 'minecraft:gold_block')
-})
--- kubejs/server_scripts/natural altar infusion/tainted_gold.js
+++
@@ -1,5 +0,0 @@
-ServerEvents.recipes((event) => {
-    // 'naturesaura:tainted_gold' 'minecraft:gold_ingot'
-    event.remove({input: 'minecraft:gold_ingot', output: 'naturesaura:tainted_gold' })
-    event.recipes.naturesaura.altar('naturesaura:tainted_gold', 'minecraft:gold_ingot')
-})
