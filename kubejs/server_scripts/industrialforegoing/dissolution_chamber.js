0df398c5d859d5b1aace672172bdb1293b6bf7a2
KCtops100 <kylecollamer@gmail.com>
2024-12-28 15:53:39 -0500

    Added Recipes for Nether and End Addons.

---
+++ kubejs/server_scripts/industrialforegoing/dissolution_chamber.js
@@ -0,0 +1,74 @@
+ServerEvents.recipes(event => {
+    // nether addon
+    event.custom({
+        type: "industrialforegoing:dissolution_chamber",
+        input: [
+            {
+                item: 'minecraft:wither_skeleton_skull'
+            },
+            {
+                item: 'minecraft:nether_star'
+            },
+            {
+                item: 'minecraft:diamond'
+            },
+            {
+                item: 'minecraft:diamond'
+            },
+            {
+                item: "kubejs:infinitium_ingot"
+            },
+            {
+                item: "kubejs:infinitium_ingot"
+            },
+            {
+                item: 'extendedcrafting:enhanced_redstone_catalyst'
+            },
+            {
+                item: 'extendedcrafting:enhanced_redstone_catalyst'
+            }
+        ],
+        inputFluid: "{Amount:1000,FluidName:\"kubejs:liquid_infinitium\"}",
+        output: {
+            count: 1,
+            item: "kubejs:nether_addon",
+        },
+        processingTime: 1000
+    });
+    // end addon
+    event.custom({
+        type: "industrialforegoing:dissolution_chamber",
+        input: [
+            {
+                item: 'minecraft:dragon_egg'
+            },
+            {
+                item: 'minecraft:dragon_breath'
+            },
+            {
+                item: 'minecraft:diamond'
+            },
+            {
+                item: 'minecraft:diamond'
+            },
+            {
+                item: "kubejs:infinitium_ingot"
+            },
+            {
+                item: "kubejs:infinitium_ingot"
+            },
+            {
+                item: 'extendedcrafting:enhanced_ender_catalyst'
+            },
+            {
+                item: 'extendedcrafting:enhanced_ender_catalyst'
+            }
+        ],
+        inputFluid: "{Amount:2000,FluidName:\"kubejs:liquid_infinitium\"}",
+        output: {
+            count: 1,
+            item: "kubejs:end_addon",
+        },
+        processingTime: 2000
+    });
+});
