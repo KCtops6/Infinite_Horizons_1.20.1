168f84ebc06521e59542b07879479df11404e95d
KCtops100 <kylecollamer@gmail.com>
2024-12-28 15:16:02 -0500

    added liquid infinitium

--- kubejs/startup_scripts/fluids.js
+++ kubejs/startup_scripts/fluids.js
@@ -1,3 +1,4 @@
 StartupEvents.registry('fluid', event => {
     event.create('liquid_ender_pearl').thickTexture(0x032620).bucketColor(0x032620).displayName('Liquid Ender Pearl').noBlock()
+    event.create('liquid_infinitium').thickTexture(0x3b3e41).bucketColor(0x3b3e41).displayName('Liquid Infinitium').noBlock()
 })
