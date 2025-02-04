d791c4d192e2a00bc1be4d93274a46a677240c84
KCtops100 <kylecollamer@gmail.com>
2025-02-04 10:07:19 -0500

    Reformatted Resin Bee Villager Trades.

--- kubejs/server_scripts/custom_villager_trades.js
+++ kubejs/server_scripts/custom_villager_trades.js
@@ -266,60 +266,22 @@ MoreJSEvents.villagerTrades((event) => {
             input2: 'productivebees:honey_treat',
             output: 'productivebees:spawn_egg_yellow_black_carpenter_bee'
         },
+        // Resin Bee
+        {
+            level: 1,
+            input1: 'productivebees:spruce_wood_nest',
+            input2: 'productivebees:honey_treat',
+            output: 'productivebees:spawn_egg_resin_bee'
+        },
     ];
 
     apiaristTrades.forEach(trade => {
-        event.addTrade("spacecatcustomprofessions:apiarist", level, [input1, input2], output);
-    })
-    event.addTrade(
-        "spacecatcustomprofessions:apiarist", 1,    // Profession and level.
-        ['productivebees:oak_wood_nest', TradeItem.of('productivebees:honey_treat', 1, 3)],  // Input item(s).
-        Item.of('productivebees:sturdy_bee_cage','{Age:0,AngerTime:0,BalmData:{},CanUpdate:1b,ForcedAge:0,ForgeCaps:{"curios:inventory":{Curios:[]},"mekanism:radiation":{radiation:1.0E-7d}},ForgeData:{"naturesaura:time_alive":40},HasConverted:0b,HasNectar:0b,HasStung:0b,Health:10.0f,Invulnerable:0b,MaxHealth:10.0f,PersistenceRequired:0b,bee_behavior:0,bee_endurance:1,bee_productivity:2,bee_temper:1,bee_type:"solitary",bee_weather_tolerance:0,entity:"productivebees:green_carpenter_bee","forge:spawn_type":"SPAWN_EGG",isProductiveBee:1b,mod:"ProductiveBees",name:"Green Carpenter Bee"}')  // Output item.
-    );
-    event.addTrade(
-        "spacecatcustomprofessions:apiarist", 1,    // Profession and level.
-        ['productivebees:oak_wood_nest', TradeItem.of('productivebees:honey_treat', 1, 3)],  // Input item(s).
-        Item.of('productivebees:sturdy_bee_cage', '{Age:0,AngerTime:0,BalmData:{},CanUpdate:1b,ForcedAge:0,ForgeCaps:{"curios:inventory":{Curios:[]},"mekanism:radiation":{radiation:1.0E-7d}},ForgeData:{},HasConverted:0b,HasNectar:0b,HasStung:0b,Health:15.0f,Invulnerable:0b,MaxHealth:15.0f,PersistenceRequired:0b,bee_behavior:0,bee_endurance:2,bee_productivity:1,bee_temper:1,bee_type:"solitary",bee_weather_tolerance:0,entity:"productivebees:yellow_black_carpenter_bee","forge:spawn_type":"SPAWN_EGG",isProductiveBee:1b,mod:"ProductiveBees",name:"Yellow Carpenter Bee"}')  // Output item.
-    );
-    event.addTrade(
-        "spacecatcustomprofessions:apiarist", 1,    // Profession and level.
-        ['productivebees:spruce_wood_nest', TradeItem.of('productivebees:honey_treat', 1, 3)],  // Input item(s).
-        Item.of('productivebees:sturdy_bee_cage', '{Age:0,AngerTime:0,BalmData:{},CanUpdate:1b,ForcedAge:0,ForgeCaps:{"curios:inventory":{Curios:[]},"mekanism:radiation":{radiation:1.0E-7d}},ForgeData:{},HasConverted:0b,HasNectar:0b,HasStung:0b,Health:10.0f,Invulnerable:0b,MaxHealth:10.0f,PersistenceRequired:0b,bee_behavior:0,bee_endurance:1,bee_productivity:1,bee_temper:1,bee_type:"solitary",bee_weather_tolerance:0,entity:"productivebees:resin_bee","forge:spawn_type":"SPAWN_EGG",isProductiveBee:1b,mod:"ProductiveBees",name:"Resin Bee"}')  // Output item.
-    );
-    event.addTrade(
-        "spacecatcustomprofessions:apiarist", 1,    // Profession and level.
-        ['productivebees:spruce_wood_nest', TradeItem.of('productivebees:honey_treat', 1, 3)],  // Input item(s).
-        Item.of('productivebees:sturdy_bee_cage', '{Age:0,AngerTime:0,BalmData:{},CanUpdate:1b,ForcedAge:0,ForgeCaps:{"curios:inventory":{Curios:[]},"mekanism:radiation":{radiation:1.0E-7d}},ForgeData:{},HasConverted:0b,HasNectar:0b,HasStung:0b,Health:15.0f,Invulnerable:0b,MaxHealth:15.0f,PersistenceRequired:0b,bee_behavior:0,bee_endurance:2,bee_productivity:1,bee_temper:1,bee_type:"solitary",bee_weather_tolerance:0,entity:"productivebees:yellow_black_carpenter_bee","forge:spawn_type":"SPAWN_EGG",isProductiveBee:1b,mod:"ProductiveBees",name:"Yellow Carpenter Bee"}')  // Output item.
-    );
-    event.addTrade(
-        "spacecatcustomprofessions:apiarist", 1,    // Profession and level.
-        ['productivebees:dark_oak_wood_nest', TradeItem.of('productivebees:honey_treat', 1, 3)],  // Input item(s).
-        Item.of('productivebees:sturdy_bee_cage', '{Age:0,AngerTime:0,BalmData:{},CanUpdate:1b,ForcedAge:0,ForgeCaps:{"curios:inventory":{Curios:[]},"mekanism:radiation":{radiation:1.0E-7d}},ForgeData:{"naturesaura:time_alive":40},HasConverted:0b,HasNectar:0b,HasStung:0b,Health:10.0f,Invulnerable:0b,MaxHealth:10.0f,PersistenceRequired:0b,bee_behavior:0,bee_endurance:1,bee_productivity:2,bee_temper:1,bee_type:"solitary",bee_weather_tolerance:0,entity:"productivebees:green_carpenter_bee","forge:spawn_type":"SPAWN_EGG",isProductiveBee:1b,mod:"ProductiveBees",name:"Green Carpenter Bee"}')  // Output item.
-    );
+        event.addTrade("spacecatcustomprofessions:apiarist", trade.level, [trade.input1, trade.input2], trade.output);
+    });
     event.addTrade(
         "spacecatcustomprofessions:apiarist", 1,    // Profession and level.
         ['productivebees:dark_oak_wood_nest', TradeItem.of('productivebees:honey_treat', 1, 3)],  // Input item(s).
         Item.of('productivebees:sturdy_bee_cage', '{Age:0,AngerTime:0,BalmData:{},CanUpdate:1b,ForcedAge:0,ForgeCaps:{"curios:inventory":{Curios:[]},"mekanism:radiation":{radiation:1.0E-7d}},ForgeData:{},HasConverted:0b,HasNectar:0b,HasStung:0b,Health:15.0f,Invulnerable:0b,MaxHealth:15.0f,PersistenceRequired:0b,bee_behavior:0,bee_endurance:2,bee_productivity:0,bee_temper:1,bee_type:"solitary",bee_weather_tolerance:0,entity:"productivebees:blue_banded_bee","forge:spawn_type":"SPAWN_EGG",isProductiveBee:1b,mod:"ProductiveBees",name:"Blue Banded Bee"}')  // Output item.
-    );
-    event.addTrade(
-        "spacecatcustomprofessions:apiarist", 1,    // Profession and level.
-        ['productivebees:dark_oak_wood_nest', TradeItem.of('productivebees:honey_treat', 1, 3)],  // Input item(s).
-        Item.of('productivebees:sturdy_bee_cage', '{Age:0,AngerTime:0,BalmData:{},CanUpdate:1b,ForcedAge:0,ForgeCaps:{"curios:inventory":{Curios:[]},"mekanism:radiation":{radiation:1.0E-7d}},ForgeData:{},HasConverted:0b,HasNectar:0b,HasStung:0b,Health:15.0f,Invulnerable:0b,MaxHealth:15.0f,PersistenceRequired:0b,bee_behavior:0,bee_endurance:2,bee_productivity:1,bee_temper:1,bee_type:"solitary",bee_weather_tolerance:0,entity:"productivebees:yellow_black_carpenter_bee","forge:spawn_type":"SPAWN_EGG",isProductiveBee:1b,mod:"ProductiveBees",name:"Yellow Carpenter Bee"}')  // Output item.
-    );
-    event.addTrade(
-        "spacecatcustomprofessions:apiarist", 1,    // Profession and level.
-        ['productivebees:birch_wood_nest', TradeItem.of('productivebees:honey_treat', 1, 3)],  // Input item(s).
-        Item.of('productivebees:sturdy_bee_cage', '{Age:0,AngerTime:0,BalmData:{},CanUpdate:1b,ForcedAge:0,ForgeCaps:{"curios:inventory":{Curios:[]},"mekanism:radiation":{radiation:1.0E-7d}},ForgeData:{"naturesaura:time_alive":40},HasConverted:0b,HasNectar:0b,HasStung:0b,Health:10.0f,Invulnerable:0b,MaxHealth:10.0f,PersistenceRequired:0b,bee_behavior:0,bee_endurance:1,bee_productivity:2,bee_temper:1,bee_type:"solitary",bee_weather_tolerance:0,entity:"productivebees:green_carpenter_bee","forge:spawn_type":"SPAWN_EGG",isProductiveBee:1b,mod:"ProductiveBees",name:"Green Carpenter Bee"}')  // Output item.
-    );
-    event.addTrade(
-        "spacecatcustomprofessions:apiarist", 1,    // Profession and level.
-        ['productivebees:birch_wood_nest', TradeItem.of('productivebees:honey_treat', 1, 3)],  // Input item(s).
-        Item.of('productivebees:sturdy_bee_cage', '{Age:0,AngerTime:0,BalmData:{},CanUpdate:1b,ForcedAge:0,ForgeCaps:{"curios:inventory":{Curios:[]},"mekanism:radiation":{radiation:1.0E-7d}},ForgeData:{},HasConverted:0b,HasNectar:0b,HasStung:0b,Health:15.0f,Invulnerable:0b,MaxHealth:15.0f,PersistenceRequired:0b,bee_behavior:0,bee_endurance:2,bee_productivity:1,bee_temper:1,bee_type:"solitary",bee_weather_tolerance:0,entity:"productivebees:yellow_black_carpenter_bee","forge:spawn_type":"SPAWN_EGG",isProductiveBee:1b,mod:"ProductiveBees",name:"Yellow Carpenter Bee"}')  // Output item.
-    );
-    event.addTrade(
-        "spacecatcustomprofessions:apiarist", 1,    // Profession and level.
-        ['productivebees:jungle_wood_nest', TradeItem.of('productivebees:honey_treat', 1, 3)],  // Input item(s).
-        Item.of('productivebees:sturdy_bee_cage', '{Age:0,AngerTime:0,BalmData:{},CanUpdate:1b,ForcedAge:0,ForgeCaps:{"curios:inventory":{Curios:[]},"mekanism:radiation":{radiation:1.0E-7d}},ForgeData:{"naturesaura:time_alive":40},HasConverted:0b,HasNectar:0b,HasStung:0b,Health:10.0f,Invulnerable:0b,MaxHealth:10.0f,PersistenceRequired:0b,bee_behavior:0,bee_endurance:1,bee_productivity:2,bee_temper:1,bee_type:"solitary",bee_weather_tolerance:0,entity:"productivebees:green_carpenter_bee","forge:spawn_type":"SPAWN_EGG",isProductiveBee:1b,mod:"ProductiveBees",name:"Green Carpenter Bee"}')  // Output item.
     );
     event.addTrade(
         "spacecatcustomprofessions:apiarist", 1,    // Profession and level.
@@ -330,11 +292,6 @@ MoreJSEvents.villagerTrades((event) => {
         "spacecatcustomprofessions:apiarist", 1,    // Profession and level.
         ['productivebees:cherry_wood_nest', TradeItem.of('productivebees:honey_treat', 1, 3)],  // Input item(s).
         Item.of('productivebees:sturdy_bee_cage', '{Age:0,AngerTime:0,BalmData:{},CanUpdate:1b,ForcedAge:0,ForgeCaps:{"curios:inventory":{Curios:[]},"mekanism:radiation":{radiation:1.0E-7d}},ForgeData:{},HasConverted:0b,HasNectar:0b,HasStung:0b,Health:15.0f,Invulnerable:0b,MaxHealth:15.0f,PersistenceRequired:0b,bee_behavior:0,bee_endurance:2,bee_productivity:0,bee_temper:1,bee_type:"solitary",bee_weather_tolerance:0,entity:"productivebees:blue_banded_bee","forge:spawn_type":"SPAWN_EGG",isProductiveBee:1b,mod:"ProductiveBees",name:"Blue Banded Bee"}')  // Output item.
-    );
-    event.addTrade(
-        "spacecatcustomprofessions:apiarist", 1,    // Profession and level.
-        ['productivebees:mangrove_wood_nest', TradeItem.of('productivebees:honey_treat', 1, 3)],  // Input item(s).
-        Item.of('productivebees:sturdy_bee_cage', '{Age:0,AngerTime:0,BalmData:{},CanUpdate:1b,ForcedAge:0,ForgeCaps:{"curios:inventory":{Curios:[]},"mekanism:radiation":{radiation:1.0E-7d}},ForgeData:{"naturesaura:time_alive":40},HasConverted:0b,HasNectar:0b,HasStung:0b,Health:10.0f,Invulnerable:0b,MaxHealth:10.0f,PersistenceRequired:0b,bee_behavior:0,bee_endurance:1,bee_productivity:2,bee_temper:1,bee_type:"solitary",bee_weather_tolerance:0,entity:"productivebees:green_carpenter_bee","forge:spawn_type":"SPAWN_EGG",isProductiveBee:1b,mod:"ProductiveBees",name:"Green Carpenter Bee"}')  // Output item.
     );
     event.addTrade(
         "spacecatcustomprofessions:apiarist", 2,    // Profession and level.
