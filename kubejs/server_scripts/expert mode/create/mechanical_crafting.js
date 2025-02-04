51264d6b753767425cf17a69ffd961e5e75530d4
KCtops100 <kylecollamer@gmail.com>
2025-02-04 15:10:15 -0500

    Added Expert Mode Material Stonework Factory Recipe.

---
+++ kubejs/server_scripts/expert mode/create/mechanical_crafting.js
@@ -0,0 +1,53 @@
+//packmode: expert
+ServerEvents.recipes(event => {
+    // Material Stonework Factory
+    event.remove({ id: "industrialforegoing:material_stonework_factory" });
+    event.custom({
+        type: "create:mechanical_crafting",
+        acceptMirrored: false,
+        key: {
+            A: {
+                item: "industrialforegoing:machine_frame_advanced"
+            },
+            B: {
+                item: "thermal:machine_furnace"
+            },
+            C: {
+                item: "thermal:machine_pulverizer"
+            },
+            D: {
+                item: "thermal:machine_crafter"
+            },
+            E: {
+                item: "thermal:device_rock_gen"
+            },
+            F: {
+                item: "industrialforegoing:pink_slime_ingot"
+            },
+            G: {
+                item: "kubejs:electrum_gear"
+            },
+            H: {
+                item: "thermal:obsidian_glass"
+            },
+            I: {
+                item: "industrialforegoing:plastic"
+            },
+            J: {
+                item: "kubejs:invar_plate"
+            }
+        },
+        pattern: [
+            "IIIII",
+            "IFDGI",
+            "HCABH",
+            "JGEFJ",
+            "JJJJJ"
+        ],
+        result: {
+            count: 1,
+            item: "industrialforegoing:material_stonework_factory"
+        }
+    });
+
+});
