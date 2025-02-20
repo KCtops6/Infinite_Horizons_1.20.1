456fdf5f608a5e1fb90bf726143186a7417e3b98
KCtops100 <kylecollamer@gmail.com>
2025-02-20 13:07:49 -0500

    Fixed create milling recipe for sand.

--- kubejs/server_scripts/normal mode/create/create_milling.js
+++ kubejs/server_scripts/normal mode/create/create_milling.js
@@ -1,75 +1,46 @@
 ServerEvents.recipes(event => {
-    // simple crushing input to output mappings (no chanced outputs)
+    // Simple crushing input to output mappings (no chanced outputs)
     const recipes = [
         { input: 'minecraft:sand', output: 'createsifter:dust' },
-        { input: 'minecraft:gravel', output: 'createsifter:sand' },
+        { input: 'minecraft:gravel', output: 'minecraft:sand' },
         { input: 'minecraft:netherrack', output: 'kubejs:netherrack_gravel' },
         { input: 'minecraft:basalt', output: 'kubejs:basalt_gravel' },
         { input: 'minecraft:ender_pearl', output: 'kubejs:ender_pearl_dust' }
-    ]
+    ];
+
     recipes.forEach(recipe => {
         event.custom({
             type: "create:milling",
-            ingredients: [
-                {
-                    item: recipe.input
-                }
-            ],
+            ingredients: [{ item: recipe.input }],
             processingTime: 800,
-            results: [
-                {
-                    item: recipe.output
-                }
-            ]
+            results: [{ item: recipe.output }]
         });
     });
-    
-    // pams harvestcraft ground meats
+
+    // Pam's HarvestCraft ground meats
     const meats = ['rabbit', 'mutton', 'pork', 'beef', 'chicken'];
     meats.forEach(meat => {
         event.custom({
             type: "create:milling",
-            ingredients: [
-                {
-                    item: `#forge:raw${meat}`
-                }
-            ],
+            ingredients: [{ tag: `forge:raw_${meat}` }],
             processingTime: 800,
-            results: [
-                {
-                    item: `pamhc2foodcore:ground${meat}item`
-                }
-            ]
+            results: [{ item: `pamhc2foodcore:ground${meat}item` }]
         });
     });
+
+    // Fish milling
     event.custom({
         type: "create:milling",
-        ingredients: [
-            {
-                item: `#forge:fishes`
-            }
-        ],
+        ingredients: [{ tag: 'forge:fishes' }],
         processingTime: 800,
-        results: [
-            {
-                item: `pamhc2foodcore:groundfishitem`
-            }
-        ]
+        results: [{ item: 'pamhc2foodcore:groundfishitem' }]
     });
 
-    // calcite
+    // Calcite milling
     event.custom({
         type: "create:milling",
-        ingredients: [
-            {
-                item: 'minecraft:calcite'
-            }
-        ],
+        ingredients: [{ item: 'minecraft:calcite' }],
         processingTime: 800,
-        results: [
-            {
-                item: 'gtceu:raw_calcite'
-            }
-        ]
+        results: [{ item: 'gtceu:raw_calcite' }]
     });
 });
