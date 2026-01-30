a067315d62e7fb92eaa778d7a454221f3ff78723
KCtops6 <kctops06@gmail.com>
2026-01-30 12:39:35 -0500

    Fixed coal coke tags

--- kubejs/server_scripts/normal mode/server-events/Tags/item.js
+++ kubejs/server_scripts/normal mode/server-events/Tags/item.js
@@ -47,44 +47,55 @@ ServerEvents.tags('item', event => {
     let stone_pebbles = ['twigs:pebble', 'botania:pebble', 'kubejs:stone_pebble'];
     stone_pebbles.forEach(s => {
         event.add('kubejs:pebbles', s);
-    })
+    });
     let gearboxes = ['create:gearbox', 'create:vertical_gearbox']
     gearboxes.forEach(g => {
         event.add('create:gearboxes', g)
-    })
+    });
     let copper_tools = [
         'create_sa:copper_pickaxe', 'create_sa:copper_axe', 'create_sa:copper_sword',
         'create_sa:copper_shovel', 'create_sa:copper_hoe'
-    ]
+    ];
     copper_tools.forEach(t => {
-        event.add('create:copper_tools', t)
-    })
+        event.add('create:copper_tools', t);
+    });
     let copper_armor = [
         'create_sa:copper_helmet', 'create_sa:copper_chestplate',
         'create_sa:copper_leggings', 'create_sa:copper_boots'
-    ]
+    ];
     copper_armor.forEach(a => {
-        event.add('create:copper_armor', a)
-    })
+        event.add('create:copper_armor', a);
+    });
     let lapis_armor = [
         'mekanismtools:lapis_lazuli_helmet', 'mekanismtools:lapis_lazuli_chestplate',
         'mekanismtools:lapis_lazuli_leggings', 'mekanismtools:lapis_lazuli_boots'
-    ]
+    ];
     lapis_armor.forEach(a => {
-        event.add('create:lapis_armor', a)
-    })
+        event.add('create:lapis_armor', a);
+    });
     let lapis_tools = [
         'mekanismtools:lapis_lazuli_pickaxe', 'mekanismtools:lapis_lazuli_axe', 'mekanismtools:lapis_lazuli_shovel',
         'mekanismtools:lapis_lazuli_hoe', 'mekanismtools:lapis_lazuli_sword'
-    ]
+    ];
     lapis_tools.forEach(t => {
-        event.add('create:lapis_tools', t)
-    })
-    event.add('forge:coal_coke', 'kubejs:coal_coke')
+        event.add('create:lapis_tools', t);
+    });
+
+    event.add('forge:coal_coke', 'kubejs:coal_coke');
+    event.add('forge:storage_blocks/coal_coke', 'kubejs:coal_coke_block');
+    event.remove('forge:coal_coke', [
+        'immersiveengineering:coal_coke',
+        'thermal:coal_coke'
+    ]);
+    event.remove('forge:storage_blocks/coal_coke', [
+        'immersiveengineering:coke',
+        'thermal:coal_coke_block'
+    ]);
+
     global.raw_ore_blocks.forEach(b => {
-        event.add(`forge:storage_blocks/raw_${b}`, `kubejs:block_of_raw_${b}`)
-    })
+        event.add(`forge:storage_blocks/raw_${b}`, `kubejs:block_of_raw_${b}`);
+    });
     global.raw_ores.forEach(r => {
-        event.add(`forge:raw_materials/${r}`, `kubejs:raw_${r}`)
-    })
-})
+        event.add(`forge:raw_materials/${r}`, `kubejs:raw_${r}`);
+    });
+});
