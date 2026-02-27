1cf85448b2c6fa1bbb866ba47820781898abc885
KCtops6 <kctops06@gmail.com>
2026-02-27 15:05:46 -0500

    Added raw uraninite (Powah) to Create Sifting (deepslate).

--- kubejs/server_scripts/normal mode/server-events/recipes/create/sifting.js
+++ kubejs/server_scripts/normal mode/server-events/recipes/create/sifting.js
@@ -233,8 +233,9 @@ ServerEvents.recipes(event => {
         Item.of('kubejs:raw_osmium').withChance(0.25),
         Item.of('kubejs:raw_silver').withChance(0.125),
         Item.of('minecraft:diamond').withChance(0.0625),
+        Item.of('powah:uraninite_raw').withChance(0.0625),
         Item.of('irons_spellbooks:raw_mithril').withChance(0.0625)
-    ], ['kubejs:deepslate_gravel','kubejs:iron_mesh']).id('kubejs/recipes/createsifterSifting/iron/deepslate')
+    ], ['kubejs:deepslate_gravel','kubejs:iron_mesh']).id('kubejs/recipes/createsifterSifting/iron/deepslate');
     event.recipes.createsifterSifting([
         Item.of('minecraft:raw_gold').withChance(0.375),
         Item.of('kubejs:raw_aluminum').withChance(0.125),
@@ -244,8 +245,9 @@ ServerEvents.recipes(event => {
         Item.of('kubejs:raw_silver').withChance(0.25),
         Item.of('kubejs:raw_uranium').withChance(0.125),
         Item.of('minecraft:diamond').withChance(0.125),
+        Item.of('powah:uraninite_raw').withChance(0.125),
         Item.of('irons_spellbooks:raw_mithril').withChance(0.125)
-    ], ['kubejs:deepslate_gravel','createsifter:brass_mesh']).id('kubejs/recipes/createsifterSifting/brass/deepslate')
+    ], ['kubejs:deepslate_gravel','createsifter:brass_mesh']).id('kubejs/recipes/createsifterSifting/brass/deepslate');
     event.recipes.createsifterSifting([
         Item.of('minecraft:raw_gold').withChance(0.5),
         Item.of('kubejs:raw_aluminum').withChance(0.25),
@@ -255,8 +257,12 @@ ServerEvents.recipes(event => {
         Item.of('kubejs:raw_silver').withChance(0.5),
         Item.of('kubejs:raw_uranium').withChance(0.25),
         Item.of('minecraft:diamond').withChance(0.25),
+        Item.of('powah:uraninite_raw').withChance(0.25),
         Item.of('irons_spellbooks:raw_mithril').withChance(0.25)
-    ], ['kubejs:deepslate_gravel','kubejs:netherite_mesh']).id('kubejs/recipes/createsifterSifting/netherite/deepslate')
+    ], ['kubejs:deepslate_gravel','kubejs:netherite_mesh']).id('kubejs/recipes/createsifterSifting/netherite/deepslate');
+
+
+
     event.recipes.createsifterSifting([
         Item.of('gtceu:raw_graphite').withChance(0.25),
         Item.of('gtceu:raw_diamond').withChance(0.125),
@@ -266,7 +272,7 @@ ServerEvents.recipes(event => {
         Item.of('gtceu:raw_nickel').withChance(0.125),
         Item.of('gtceu:raw_cobaltite').withChance(0.125),
         Item.of('gtceu:raw_pentlandite').withChance(0.0625)
-    ], ['kubejs:deepslate_gravel','kubejs:bronze_mesh']).id('kubejs/recipes/createsifterSifting/bronze/soul_sand')
+    ], ['kubejs:deepslate_gravel','kubejs:bronze_mesh']).id('kubejs/recipes/createsifterSifting/bronze/soul_sand');
 
     /**
      * Limestone (gravel)
