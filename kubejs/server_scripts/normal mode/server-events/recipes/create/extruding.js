1b77806d06bda384e3583d5327065d7a35a7d683
KCtops6 <kctops06@gmail.com>
2026-03-13 12:43:45 -0400

    Add extruding recipes for stone variants in mechanical extruder

--- kubejs/server_scripts/normal mode/server-events/recipes/create/extruding.js
+++ kubejs/server_scripts/normal mode/server-events/recipes/create/extruding.js
@@ -2,8 +2,17 @@ ServerEvents.recipes(event => {
     event.remove({ type: 'create_mechanical_extruder:extruding' })
     event.recipes.create_mechanical_extruder.extruding(Item.of('minecraft:cobblestone'),
         [ Fluid.of('minecraft:lava'), Fluid.of('minecraft:water') ]
-    ).id('kubejs/create_mechanical_extruder/cobblestone')
+    ).id('kubejs/create_mechanical_extruder/cobblestone');
     event.recipes.create_mechanical_extruder.extruding(Item.of('minecraft:stone'),
         [ Fluid.of('minecraft:lava'), Fluid.of('minecraft:water') ]
-    ).id('kubejs/create_mechanical_extruder/stone')
-})
+    ).catalyst('minecraft:stone').requiredBonks(2).id('kubejs/create_mechanical_extruder/stone');
+    event.recipes.create_mechanical_extruder.extruding(Item.of('minecraft:andesite'),
+        [ Fluid.of('minecraft:lava'), Fluid.of('minecraft:water') ]
+    ).catalyst('minecraft:andesite').requiredBonks(2).id('kubejs/create_mechanical_extruder/andesite');
+    event.recipes.create_mechanical_extruder.extruding(Item.of('minecraft:diorite'),
+        [ Fluid.of('minecraft:lava'), Fluid.of('minecraft:water') ]
+    ).catalyst('minecraft:diorite').requiredBonks(2).id('kubejs/create_mechanical_extruder/diorite');
+    event.recipes.create_mechanical_extruder.extruding(Item.of('minecraft:granite'),
+        [ Fluid.of('minecraft:lava'), Fluid.of('minecraft:water') ]
+    ).catalyst('minecraft:granite').requiredBonks(2).id('kubejs/create_mechanical_extruder/granite');
+});
