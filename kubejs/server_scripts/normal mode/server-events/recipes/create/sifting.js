b22c4d30ae0d786e47674032a5069c52e0c26773
KCtops6 <kctops06@gmail.com>
2026-03-15 18:01:53 -0400

    Add sifting recipes for various seeds using dirt

--- kubejs/server_scripts/normal mode/server-events/recipes/create/sifting.js
+++ kubejs/server_scripts/normal mode/server-events/recipes/create/sifting.js
@@ -421,4 +421,17 @@ ServerEvents.recipes(event => {
             Item.of('tconstruct:ender_slime_sapling').withChance(0.05 * m.multiplier)
         ], ['tconstruct:ender_slime_dirt', m.mesh])
     })
+
+    event.recipes.createsifterSifting([
+        Item.of('minecraft:wheat_seeds').withChance(0.5),
+        Item.of('minecraft:pumpkin_seeds').withChance(0.2),
+        Item.of('minecraft:melon_seeds').withChance(0.2),
+        Item.of('farmersdelight:cabbage_seeds').withChance(0.1),
+        Item.of('farmersdelight:tomato_seeds').withChance(0.1),
+        Item.of('supplementaries:flax_seeds').withChance(0.1),
+        Item.of('delightful:cantaloupe_seeds').withChance(0.1),
+        Item.of('delightful:salmonberry_pips').withChance(0.1),
+        Item.of('occultism:datura_seeds').withChance(0.05),
+        Item.oF('immersiveengineering:seed').withChance(0.15)
+    ], ['createsifter:string_mesh', 'minecraft:dirt']).id('kubejs:createsifterSifting/string/dirt');
 })
