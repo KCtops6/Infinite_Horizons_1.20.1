84031bf82391c8b490c0c8c9d9122df9be140e9d
KCtops6 <kctops06@gmail.com>
2026-03-15 18:11:38 -0400

    Add sifting recipes for moss block with various plant drops

--- kubejs/server_scripts/normal mode/server-events/recipes/create/sifting.js
+++ kubejs/server_scripts/normal mode/server-events/recipes/create/sifting.js
@@ -435,4 +435,12 @@ ServerEvents.recipes(event => {
         Item.of('occultism:datura_seeds').withChance(0.05),
         Item.oF('immersiveengineering:seed').withChance(0.15)
     ], ['createsifter:string_mesh', 'minecraft:dirt']).id('kubejs:createsifterSifting/string/dirt');
+
+    event.recipes.createsifterSifting([
+        Item.of('minecraft:big_dripleaf').withChance(0.15),
+        Item.of('minecraft:small_dripleaf').withChance(0.2),
+        Item.of('minecraft:flowering_azalea').withChance(0.1),
+        Item.of('minecraft:azalea').withChance(0.15),
+        Item.of('minecraft:spore_blossom').withChance(0.05)
+    ], ['createsifter:string_mesh', 'minecraft:moss_block']).id('kubejs:createsifterSifting/string/moss_block');
 })
