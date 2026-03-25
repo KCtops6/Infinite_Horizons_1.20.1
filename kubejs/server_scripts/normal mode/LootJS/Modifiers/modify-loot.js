88c8d7b75abd23543670a8bcdd9a6fb0e0bb95c6
KCtops6 <kctops06@gmail.com>
2026-03-25 14:33:00 -0400

    Replaced certain Gregtech loot items with custom equivalents.

--- kubejs/server_scripts/normal mode/LootJS/Modifiers/modify-loot.js
+++ kubejs/server_scripts/normal mode/LootJS/Modifiers/modify-loot.js
@@ -1,7 +1,7 @@
 // kubejs\server_scripts\replace-loot.js
 LootJS.modifiers(event => {
     event.addLootTableModifier(/^minecraft:chests\/.*/)
-        .modifyLoot(Ingredient.of(/gtceu:.*_(ingot|nugget)/), (itemStack) => {
+        .modifyLoot(Ingredient.of(/gtceu:.*_(ingot|nugget|gem)/), (itemStack) => {
             let originalID = itemStack.id;
             let replacementID = originalID.replace("gtceu:", "kubejs:");
             if (Item.exists(replacementID)) {
