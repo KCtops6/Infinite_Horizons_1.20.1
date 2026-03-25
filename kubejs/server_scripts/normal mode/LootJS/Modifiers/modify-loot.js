f94c2e422cc76fc846b4712a4e53dcddb6f4e05b
KCtops6 <kctops06@gmail.com>
2026-03-25 14:27:08 -0400

    rearranged code

---
+++ kubejs/server_scripts/normal mode/LootJS/Modifiers/modify-loot.js
@@ -0,0 +1,12 @@
+// kubejs\server_scripts\replace-loot.js
+LootJS.modifiers(event => {
+    event.addLootTableModifier(/^minecraft:chests\/.*/)
+        .modifyLoot(Ingredient.of(/gtceu:.*_(ingot|nugget)/), (itemStack) => {
+            let originalID = itemStack.id;
+            let replacementID = originalID.replace("gtceu:", "kubejs:");
+            if (Item.exists(replacementID)) {
+                return Item.of(replacementID).withCount(itemStack.count);
+            }
+            return itemStack;
+        });
+}); 
--- kubejs/server_scripts/normal mode/LootJS/Modifiers/replace-loot.js
+++ kubejs/server_scripts/normal mode/LootJS/Modifiers/replace-loot.js
@@ -5,13 +5,4 @@ LootJS.modifiers(event => {
         .replaceLoot('immersiveengineering:nugget_silver', 'kubejs:silver_nugget')
         .replaceLoot('immersiveengineering:nugget_lead', 'kubejs:lead_nugget')
         .replaceLoot('immersiveengineering:nugget_nickel', 'kubejs:nickel_nugget');
-    event.addLootTableModifier(/^minecraft:chests\/.*/)
-        .modifyLoot(Ingredient.of(/gtceu:.*_(ingot|nugget)/), (itemStack) => {
-            let originalID = itemStack.id;
-            let replacementID = originalID.replace("gtceu:", "kubejs:");
-            if (Item.exists(replacementID)) {
-                return Item.of(replacementID).withCount(itemStack.count);
-            }
-            return itemStack;
-        });
 }); 
