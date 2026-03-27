cb44ceb752394ac0a5266f77e00df0b1f16fb5b1
KCtops6 <kctops06@gmail.com>
2026-03-27 14:25:41 -0400

    Replaced all relevant gregtech loot items from sophisticated mods (specifically from backpacks dropped by buffed mobs) with custom equivalents

--- kubejs/server_scripts/normal mode/LootJS/Modifiers/modify-loot.js
+++ kubejs/server_scripts/normal mode/LootJS/Modifiers/modify-loot.js
@@ -1,12 +1,11 @@
 // kubejs\server_scripts\replace-loot.js
 LootJS.modifiers(event => {
-    event.addLootTableModifier(/^minecraft:chests\/.*/)
+    event.addLootTableModifier(/^(minecraft:chests\/|sophisticated\/).*/)
         .modifyLoot(Ingredient.of(/gtceu:.*_(ingot|nugget|gem)/), (itemStack) => {
-            let originalID = itemStack.id;
-            let replacementID = originalID.replace("gtceu:", "kubejs:");
+            let replacementID = itemStack.id.replace("gtceu:", "kubejs:");
             if (Item.exists(replacementID)) {
                 return Item.of(replacementID).withCount(itemStack.count);
             }
             return itemStack;
         });
-}); 
+});
