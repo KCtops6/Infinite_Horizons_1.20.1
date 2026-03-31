4ee640c6a50364bbdf7c032d56c49f17810c2d2b
KCtops6 <kctops06@gmail.com>
2026-03-31 13:36:08 -0400

    fixed ars nouveau crushing script

--- kubejs/server_scripts/normal mode/server-events/recipes/custom/ars-nouveau/crush.js
+++ kubejs/server_scripts/normal mode/server-events/recipes/custom/ars-nouveau/crush.js
@@ -1,15 +1,13 @@
 ServerEvents.recipes(event => {
-    // simple crushing input to output mappings
     const recipes = [
         { input: 'minecraft:sand', output: 'createsifter:dust'},
-        { input: 'minecraft:gravel', output: 'createsifter:sand'},
+        { input: 'minecraft:gravel', output: 'minecraft:sand'},
         { input: 'minecraft:netherrack', output: 'kubejs:netherrack_gravel'},
         { input: 'minecraft:basalt', output: 'kubejs:basalt_gravel'},
         { input: 'minecraft:end_stone', output: 'kubejs:end_stone_gravel'}
     ]
 
     recipes.forEach(recipe => {
-        // Generates a unique ID string, e.g., "ars_nouveau:crushing/sand"
         let recipeId = `ars_nouveau:crushing/${recipe.input.split(':')[1]}`
 
         event.custom({
@@ -19,14 +17,11 @@ ServerEvents.recipes(event => {
             },
             output: [
                 {
-                    chance: 1.0,
-                    maxRange: 1,
-                    stack: {
-                        count: 1,
-                        item: recipe.output
-                    }
+                    item: recipe.output,
+                    count: 1,
+                    chance: 1.0
                 }
             ]
-        }).id(recipeId); // This assigns the ID to the recipe
+        }).id(recipeId);
     });
 });
