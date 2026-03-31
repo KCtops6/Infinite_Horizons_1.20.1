66fca075e35f53954ecefea6b956283430657309
KCtops6 <kctops06@gmail.com>
2026-03-31 10:40:44 -0400

    moved villager trades file into MoreJS folder

--- kubejs/server_scripts/normal mode/MoreJS/villager-trades.js
+++
@@ -1,5 +0,0 @@
-MoreJSEvents.villagerTrades(event => {
-    event.addTrade('minecraft:fisherman', 5, [
-        TradeItem.of('minecraft:bucket'), TradeItem.of('minecraft:emerald', 3, 5)
-    ], TradeItem.of('quark:crab_bucket'));
-});
