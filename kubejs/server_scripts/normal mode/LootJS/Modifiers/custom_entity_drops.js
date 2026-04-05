93683f9c2308022e459e938b552d93648b55922d
KCtops6 <kctops06@gmail.com>
2026-04-05 19:06:07 -0400

    Added drop chance for trial cores (zombies, skeletons, creepers, spiders, enderman)

--- kubejs/server_scripts/normal mode/LootJS/Modifiers/custom_entity_drops.js
+++ kubejs/server_scripts/normal mode/LootJS/Modifiers/custom_entity_drops.js
@@ -1,4 +1,10 @@
 LootJS.modifiers((event) => {
     // Adds the ability to harvest early game wither skeleton skulls via blazing cleaver sword.
-    event.addLootTableModifier("minecraft:entities/skeleton").randomChance(0.1).matchMainHand(Item.of('create_sa:blazing_cleaver')).addLoot('minecraft:wither_skeleton_skull');
+    event.addLootTableModifier("minecraft:entities/skeleton")
+        .randomChance(0.1)
+        .matchMainHand(Item.of('create_sa:blazing_cleaver'))
+        .addLoot('minecraft:wither_skeleton_skull');
+    event.addEntityLootModifier(/^minecraft:(zombie|skeleton|creeper|spider|enderman)$/)
+        .randomChance(1)
+        .addLoot(LootEntry.of('kubejs:trial_core'));
 });
