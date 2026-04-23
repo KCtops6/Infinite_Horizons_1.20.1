35e3c5ed46647da765f02ef31567fd0bbad47629
KCtops6 <kctops06@gmail.com>
2026-04-23 12:44:03 -0400

    Fixed bug with create pressing plates

--- kubejs/server_scripts/normal mode/server-events/recipes/create/pressing.js
+++ kubejs/server_scripts/normal mode/server-events/recipes/create/pressing.js
@@ -1,27 +1,23 @@
 ServerEvents.recipes(event => {
-    // Create pressing plates
     const ingotToPlate = [
-        'brass', 'constantan', 'electrum', 'enderium', 
-        'infinitium', 'invar', 'lead',
-        'lumium', 'netherite',
-        'nickel', 'signalum', 'silver', 'tin'
-    ]
+        'brass', 'constantan', 'electrum', 'enderium', 'infinitium', 
+        'invar', 'lead', 'lumium', 'netherite', 'nickel', 'signalum', 'silver', 'tin'
+    ];
     ingotToPlate.forEach(i => {
         const getIngredient = (name) => {
             const vanilla = ['iron', 'gold', 'copper', 'netherite'];
             return vanilla.includes(name) ? `minecraft:${name}_ingot` : `kubejs:${name}_ingot`;
         };
+        const getPlate = (name) => {
+            return `kubejs:${name}_plate`;
+        };
         event.custom({
             type: "create:pressing",
             ingredients: [
-                {
-                    item: getIngredient(i)
-                }
+                { item: getIngredient(i) }
             ],
             results: [
-                {
-                    item: getIngredient(i)
-                }
+                { item: getPlate(i) }
             ]
         });
     });
