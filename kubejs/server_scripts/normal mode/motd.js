93a30be60962e1eccd542c517aeb6682332a1828
KCtops6 <kctops06@gmail.com>
2026-05-08 13:34:17 -0400

    updated motd

--- kubejs/server_scripts/normal mode/motd.js
+++ kubejs/server_scripts/normal mode/motd.js
@@ -1,8 +1,6 @@
-// PlayerEvents.loggedIn(event => {
-//     event.player.tell(Text.of(`§6§lWelcome to §aInfinite Horizons§6!`));
-//     event.player.tell(Text.darkRed('Note, the following mods will be removed in Version 11.6: ')
-//         .append(Text.red('Mantle').bold())
-//         .append(Text.darkRed(', '))
-//         .append(Text.red('Tinkers Construct').bold())
-//         .append(Text.darkRed('. Feel free to readd these mods if you enjoy them. This is a decision about refining the pack.')));
-// });
+PlayerEvents.loggedIn(event => {
+    event.player.tell(Text.of(`§6§lWelcome to §aInfinite Horizons§6!`));
+    event.player.tell(Text.darkRed('Note, the following mods will be removed in Version 12.1: ')
+        .append(Text.red('Villager Workers: Traveling Merchant Update!').bold())
+        .append(Text.darkRed('. Feel free to readd these mods if you enjoy them. This is a decision about refining the pack.')));
+});
