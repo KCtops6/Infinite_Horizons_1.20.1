ccc397222702616c7f5830e2845a7dd4e7ed1310
KCtops6 <kctops06@gmail.com>
2026-05-18 18:58:45 -0400

    Added Mekanism crusher recipe for netherrack and end stone gravel

---
+++ kubejs/server_scripts/normal mode/server-events/recipes/custom/mekanism/crushing.js
@@ -0,0 +1,25 @@
+ServerEvents.recipes(event => {
+    event.custom({
+        type: "mekanism:crushing",
+        input: {
+            ingredient: {
+                item: "minecraft:end_stone"
+            }
+        },
+        output: {
+            item: "kubejs:end_stone_gravel"
+        }
+    }).id('kubejs:mekanism/crushing/kubejs/end_stone_gravel');
+
+    event.custom({
+        type: "mekanism:crushing",
+        input: {
+            ingredient: {
+                item: "minecraft:netherrack"
+            }
+        },
+        output: {
+            item: "kubejs:netherrack_gravel"
+        }
+    }).id('kubejs:mekanism/crushing/kubejs/netherrack_gravel');
+});
