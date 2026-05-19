46a598c2c529e5ee6233965615395c2cb209bff7
KCtops6 <kctops06@gmail.com>
2026-05-19 10:24:14 -0400

    Added Material Stonework Factory (Industrial Foregoing) recipe for end stone gravel

---
+++ kubejs/server_scripts/normal mode/server-events/recipes/custom/industrial-foregoing/crusher.js
@@ -0,0 +1,20 @@
+ServerEvents.recipes(event => {
+    event.custom({
+        type: "industrialforegoing:crusher",
+        input: {
+            item: "minecraft:netherrack"
+        },
+        output: {
+            item: "kubejs:netherrack_gravel"
+        }
+    }).id('kubejs:industrialforegoing/crusher/kubejs/netherrack_gravel');
+    event.custom({
+        type: "industrialforegoing:crusher",
+        input: {
+            item: "minecraft:end_stone"
+        },
+        output: {
+            item: "kubejs:end_stone_gravel"
+        }
+    }).id('kubejs:industrialforegoing/crusher/kubejs/end_stone_gravel');
+});
