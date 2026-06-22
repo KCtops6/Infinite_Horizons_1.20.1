66d9c436c91c00efeb7c9b09024340cab31d9c06
KCtops6 <kctops06@gmail.com>
2026-06-22 15:27:39 -0400

    updated motd

--- kubejs/server_scripts/normal mode/motd.js
+++ kubejs/server_scripts/normal mode/motd.js
@@ -1,8 +1,10 @@
-// PlayerEvents.loggedIn(event => {
-//     event.player.tell(Text.of(`§6§lWelcome to §aInfinite Horizons§6!`));
-//     event.player.tell(Text.darkRed('Note, the following mods will be removed in Version 13.1: ')
-//         .append(Text.red('Villager Workers: Traveling Merchant Update!').bold())
-//         .append(Text.red(' , '))
-//         .append(Text.red('Immersive Aircraft').bold())
-//         .append(Text.darkRed('. Feel free to readd these mods if you enjoy them. This is a decision about refining the pack.')));
-// });
+PlayerEvents.loggedIn(event => {
+    event.player.tell(Text.of(`§6§lWelcome to §aInfinite Horizons§6!`));
+    event.player.tell(Text.darkRed('Note, the following mods will be removed in Version 13.1: ')
+        .append(Text.red('Amendments').bold())
+        .append(Text.red(', '))
+        .append(Text.red('Custom Villager Professions (Forge)').bold())
+        .append(Text.red(', '))
+        .append(Text.red('Environmental Materials').bold())
+        .append(Text.darkRed('. Feel free to readd these mods if you enjoy them. This is a decision about refining the pack.')));
+});
