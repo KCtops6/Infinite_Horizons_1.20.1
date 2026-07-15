1e3f7cd1901c67b5003358c18d5d60641141008c
KCtops6 <kctops06@gmail.com>
2026-07-14 21:05:29 -0400

    Temporarily removed Pam's Harvestcraft/Create compact due to Create Millstone crash

--- kubejs/server_scripts/normal mode/server-events/recipes/for-each-recipe.js
+++ kubejs/server_scripts/normal mode/server-events/recipes/for-each-recipe.js
@@ -1,74 +1,74 @@
-// kubejs\server_scripts\for-each-recipe.js
-ServerEvents.recipes(event => {
-    const GRINDER = 'pamhc2foodcore:grinderitem';
-    const MIXING_BOWL = 'pamhc2foodcore:mixingbowlitem';
-    const SAUCEPAN = 'pamhc2foodcore:saucepanitem';
-    event.forEachRecipe({ input: GRINDER }, recipe => {
-        if (recipe.getOriginalRecipeIngredients().size() == 2) {
-            let ingredientsJSON = [];
-            recipe.getOriginalRecipeIngredients().forEach(i => {
-                if (!i.test(GRINDER) && recipe.originalRecipeResult.id != 'pamhc2foodcore:flouritem') {
-                    ingredientsJSON.push(i.toJson());
-                }
-            });
-            let resultJson1 = [{
-                count: recipe.originalRecipeResult.count,
-                item: recipe.originalRecipeResult.id
-            }];
-            let resultJson2 = [{
-                count: recipe.originalRecipeResult.count * 2,
-                item: recipe.originalRecipeResult.id
-            }];
-            let cleanID = recipe.originalRecipeResult.id.replace(':', '_');
-            event.custom({
-                type: 'create:milling',
-                ingredients: ingredientsJSON,
-                results: resultJson1,
-                heatRequirement: 'heated'
-            }).id(`create:milling/kubejs/${cleanID}`);
-            event.custom({
-                type: 'create:crushing',
-                ingredients: ingredientsJSON,
-                results: resultJson2,
-                heatRequirement: 'heated'
-            }).id(`create:crushing/kubejs/${cleanID}`);
-        }
-    });
-    event.forEachRecipe({ input: MIXING_BOWL }, recipe => {
-        let ingredientsJSON = [];
-        recipe.getOriginalRecipeIngredients().forEach(i => {
-            if (!i.test(MIXING_BOWL)) {
-                ingredientsJSON.push(i.toJson());
-            }
-        });
-        let resultJson = [{
-            count: recipe.originalRecipeResult.count,
-            item: recipe.originalRecipeResult.id
-        }];
-        let cleanID = recipe.originalRecipeResult.id.replace(':', '_');
-        event.custom({
-            type: 'create:mixing',
-            ingredients: ingredientsJSON,
-            results: resultJson
-        }).id(`create:mixing/kubejs/${cleanID}`);
-    });
-    event.forEachRecipe({ input: SAUCEPAN }, recipe => {
-        let ingredientsJSON = [];
-        recipe.getOriginalRecipeIngredients().forEach(i => {
-            if (!i.test(SAUCEPAN)) {
-                ingredientsJSON.push(i.toJson());
-            }
-        });
-        let resultJson = [{
-            count: recipe.originalRecipeResult.count,
-            item: recipe.originalRecipeResult.id
-        }];
-        let cleanID = recipe.originalRecipeResult.id.replace(':', '_');
-        event.custom({
-            type: 'create:mixing',
-            ingredients: ingredientsJSON,
-            results: resultJson,
-            heatRequirement: 'heated'
-        }).id(`create:mixing/kubejs/${cleanID}`);
-    });
-});
+// // kubejs\server_scripts\for-each-recipe.js
+// ServerEvents.recipes(event => {
+//     const GRINDER = 'pamhc2foodcore:grinderitem';
+//     const MIXING_BOWL = 'pamhc2foodcore:mixingbowlitem';
+//     const SAUCEPAN = 'pamhc2foodcore:saucepanitem';
+//     event.forEachRecipe({ input: GRINDER }, recipe => {
+//         if (recipe.getOriginalRecipeIngredients().size() == 2) {
+//             let ingredientsJSON = [];
+//             recipe.getOriginalRecipeIngredients().forEach(i => {
+//                 if (!i.test(GRINDER) && recipe.originalRecipeResult.id != 'pamhc2foodcore:flouritem') {
+//                     ingredientsJSON.push(i.toJson());
+//                 }
+//             });
+//             let resultJson1 = [{
+//                 count: recipe.originalRecipeResult.count,
+//                 item: recipe.originalRecipeResult.id
+//             }];
+//             let resultJson2 = [{
+//                 count: recipe.originalRecipeResult.count * 2,
+//                 item: recipe.originalRecipeResult.id
+//             }];
+//             let cleanID = recipe.originalRecipeResult.id.replace(':', '_');
+//             event.custom({
+//                 type: 'create:milling',
+//                 ingredients: ingredientsJSON,
+//                 results: resultJson1,
+//                 heatRequirement: 'heated'
+//             }).id(`create:milling/kubejs/${cleanID}`);
+//             event.custom({
+//                 type: 'create:crushing',
+//                 ingredients: ingredientsJSON,
+//                 results: resultJson2,
+//                 heatRequirement: 'heated'
+//             }).id(`create:crushing/kubejs/${cleanID}`);
+//         }
+//     });
+//     event.forEachRecipe({ input: MIXING_BOWL }, recipe => {
+//         let ingredientsJSON = [];
+//         recipe.getOriginalRecipeIngredients().forEach(i => {
+//             if (!i.test(MIXING_BOWL)) {
+//                 ingredientsJSON.push(i.toJson());
+//             }
+//         });
+//         let resultJson = [{
+//             count: recipe.originalRecipeResult.count,
+//             item: recipe.originalRecipeResult.id
+//         }];
+//         let cleanID = recipe.originalRecipeResult.id.replace(':', '_');
+//         event.custom({
+//             type: 'create:mixing',
+//             ingredients: ingredientsJSON,
+//             results: resultJson
+//         }).id(`create:mixing/kubejs/${cleanID}`);
+//     });
+//     event.forEachRecipe({ input: SAUCEPAN }, recipe => {
+//         let ingredientsJSON = [];
+//         recipe.getOriginalRecipeIngredients().forEach(i => {
+//             if (!i.test(SAUCEPAN)) {
+//                 ingredientsJSON.push(i.toJson());
+//             }
+//         });
+//         let resultJson = [{
+//             count: recipe.originalRecipeResult.count,
+//             item: recipe.originalRecipeResult.id
+//         }];
+//         let cleanID = recipe.originalRecipeResult.id.replace(':', '_');
+//         event.custom({
+//             type: 'create:mixing',
+//             ingredients: ingredientsJSON,
+//             results: resultJson,
+//             heatRequirement: 'heated'
+//         }).id(`create:mixing/kubejs/${cleanID}`);
+//     });
+// });
