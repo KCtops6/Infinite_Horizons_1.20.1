85a7f35d5322d6b0bfdd0b6b617fbf3ecd7d89d4
KCtops6 <kctops06@gmail.com>
2026-07-17 19:51:00 -0400

    Updated resource references in sifting recipes to use Infinite Resources namespace (not old KubeJS namespace)

--- kubejs/server_scripts/normal mode/server-events/recipes/create/sifting/gravel.js
+++ kubejs/server_scripts/normal mode/server-events/recipes/create/sifting/gravel.js
@@ -15,26 +15,26 @@ ServerEvents.recipes(event => {
     event.recipes.createsifterSifting([
         Item.of('minecraft:raw_copper').withChance(0.5),
         Item.of('minecraft:raw_iron').withChance(0.5),
-        Item.of('infinite_horizons:raw_aluminum').withChance(0.125),
-        Item.of('infinite_horizons:raw_zinc').withChance(0.25),
-        Item.of('infinite_horizons:raw_tin').withChance(0.25),
+        Item.of('infinite_resources:raw_aluminum').withChance(0.125),
+        Item.of('infinite_resources:raw_zinc').withChance(0.25),
+        Item.of('infinite_resources:raw_tin').withChance(0.25),
         Item.of('minecraft:coal').withChance(0.5)
     ], ['minecraft:gravel','kubejs:iron_mesh']).id('kubejs:recipes/createsiftersifting/iron/gravel');
     event.recipes.createsifterSifting([
         Item.of('minecraft:raw_copper').withChance(0.25),
         Item.of('minecraft:raw_iron').withChance(0.25),
-        Item.of('infinite_horizons:raw_aluminum').withChance(0.25),
+        Item.of('infinite_resources:raw_aluminum').withChance(0.25),
         Item.of('kubejs:raw_osmium').withChance(0.25),
-        Item.of('infinite_horizons:raw_tin').withChance(0.5),
-        Item.of('infinite_horizons:raw_zinc').withChance(0.5),
+        Item.of('infinite_resources:raw_tin').withChance(0.5),
+        Item.of('infinite_resources:raw_zinc').withChance(0.5),
         Item.of('minecraft:lapis_lazuli').withChance(0.25),
         Item.of('minecraft:coal').withChance(0.75),
         Item.of('irons_spellbooks:raw_mithril').withChance(0.0625)
     ], ['minecraft:gravel','createsifter:brass_mesh']).id('kubejs:recipes/createsiftersifting/brass/gravel');
     event.recipes.createsifterSifting([
-        Item.of('infinite_horizons:raw_zinc').withChance(0.25),
-        Item.of('infinite_horizons:raw_aluminum').withChance(0.375),
-        Item.of('infinite_horizons:raw_tin').withChance(0.25),
+        Item.of('infinite_resources:raw_zinc').withChance(0.25),
+        Item.of('infinite_resources:raw_aluminum').withChance(0.375),
+        Item.of('infinite_resources:raw_tin').withChance(0.25),
         Item.of('kubejs:raw_osmium').withChance(0.5),
         Item.of('minecraft:lapis_lazuli').withChance(0.5),
         Item.of('minecraft:coal').withChance(0.5),
