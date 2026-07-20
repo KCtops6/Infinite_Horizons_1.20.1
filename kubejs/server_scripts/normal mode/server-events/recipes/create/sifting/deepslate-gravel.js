75292bb39a39b275591e68400bad88f6be02c1e6
KCtops6 <kctops06@gmail.com>
2026-07-19 21:01:36 -0400

    Updated item references in deepslate gravel sifting recipes to use correct namespace

--- kubejs/server_scripts/normal mode/server-events/recipes/create/sifting/deepslate-gravel.js
+++ kubejs/server_scripts/normal mode/server-events/recipes/create/sifting/deepslate-gravel.js
@@ -1,7 +1,7 @@
 ServerEvents.recipes(event => {
     event.recipes.createsifterSifting([
-        Item.of('kubejs:deepslate_pebble').withChance(0.75),
-        Item.of('kubejs:tuff_pebble').withChance(0.5),
+        Item.of('infinite_horizons_tweaks:deepslate_pebble').withChance(0.75),
+        Item.of('infinite_horizons_tweaks:tuff_pebble').withChance(0.5),
         Item.of('minecraft:glow_lichen').withChance(0.125),
         Item.of('quark:glow_lichen_growth').withChance(0.0625),
         Item.of('twigs:petrified_lichen').withChance(0.125)
@@ -11,7 +11,7 @@ ServerEvents.recipes(event => {
         Item.of('infinite_resources:raw_aluminum').withChance(0.0625),
         Item.of('infinite_resources:raw_lead').withChance(0.125),
         Item.of('infinite_resources:raw_nickel').withChance(0.125),
-        Item.of('kubejs:raw_osmium').withChance(0.25),
+        Item.of('infinite_resources:raw_osmium').withChance(0.25),
         Item.of('infinite_resources:raw_silver').withChance(0.125),
         Item.of('minecraft:diamond').withChance(0.0625),
         Item.of('powah:uraninite_raw').withChance(0.0625),
@@ -22,9 +22,9 @@ ServerEvents.recipes(event => {
         Item.of('infinite_resources:raw_aluminum').withChance(0.125),
         Item.of('infinite_resources:raw_lead').withChance(0.25),
         Item.of('infinite_resources:raw_nickel').withChance(0.25),
-        Item.of('kubejs:raw_osmium').withChance(0.5),
+        Item.of('infinite_resources:raw_osmium').withChance(0.5),
         Item.of('infinite_resources:raw_silver').withChance(0.25),
-        Item.of('kubejs:raw_uranium').withChance(0.125),
+        Item.of('infinite_resources:raw_uranium').withChance(0.125),
         Item.of('minecraft:diamond').withChance(0.125),
         Item.of('powah:uraninite_raw').withChance(0.125),
         Item.of('irons_spellbooks:raw_mithril').withChance(0.125)
@@ -36,7 +36,7 @@ ServerEvents.recipes(event => {
         Item.of('infinite_resources:raw_nickel').withChance(0.5),
         Item.of('kubejs:raw_osmium').withChance(0.25),
         Item.of('infinite_resources:raw_silver').withChance(0.5),
-        Item.of('kubejs:raw_uranium').withChance(0.25),
+        Item.of('infinite_resources:raw_uranium').withChance(0.25),
         Item.of('minecraft:diamond').withChance(0.25),
         Item.of('powah:uraninite_raw').withChance(0.25),
         Item.of('irons_spellbooks:raw_mithril').withChance(0.25)
