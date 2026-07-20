5b0238264a096af06b52448e27f2680a388a3587
KCtops6 <kctops06@gmail.com>
2026-07-19 20:47:29 -0400

    Fixed Immersive Engineering house loot

--- kubejs/server_scripts/normal mode/LootJS/Modifiers/replace-loot.js
+++ kubejs/server_scripts/normal mode/LootJS/Modifiers/replace-loot.js
@@ -1,8 +1,8 @@
 // kubejs\server_scripts\replace-loot.js
 LootJS.modifiers(event => {
     event.addLootTableModifier('immersiveengineering:chests/engineers_house')
-        .replaceLoot('immersiveengineering:ingot_aluminum', 'kubejs:aluminum_ingot')
-        .replaceLoot('immersiveengineering:nugget_silver', 'kubejs:silver_nugget')
-        .replaceLoot('immersiveengineering:nugget_lead', 'kubejs:lead_nugget')
-        .replaceLoot('immersiveengineering:nugget_nickel', 'kubejs:nickel_nugget');
+        .replaceLoot('immersiveengineering:ingot_aluminum', 'infinite_horizons_tweaks:aluminum_ingot')
+        .replaceLoot('immersiveengineering:nugget_silver', 'infinite_horizons_tweaks:silver_nugget')
+        .replaceLoot('immersiveengineering:nugget_lead', 'infinite_horizons_tweaks:lead_nugget')
+        .replaceLoot('immersiveengineering:nugget_nickel', 'infinite_horizons_tweaks:nickel_nugget');
 }); 
