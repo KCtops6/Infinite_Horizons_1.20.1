780704cdacbfe4b38db133ec02d48aff2efe4236
KCtops6 <kctops06@gmail.com>
2026-07-20 19:26:32 -0400

    Fixed gravel sifting recipes.

--- kubejs/server_scripts/normal mode/server-events/recipes/create/sifting/gravel.js
+++ kubejs/server_scripts/normal mode/server-events/recipes/create/sifting/gravel.js
@@ -1,10 +1,10 @@
 ServerEvents.recipes(event => {
     event.recipes.createsifterSifting([
         Item.of('minecraft:flint').withChance(0.5),
-        Item.of('kubejs:stone_pebble').withChance(0.75),
-        Item.of('kubejs:andesite_pebble').withChance(0.5),
-        Item.of('kubejs:diorite_pebble').withChance(0.5),
-        Item.of('kubejs:granite_pebble').withChance(0.5)
+        Item.of('infinite_horizons_tweaks:stone_pebble').withChance(0.75),
+        Item.of('infinite_horizons_tweaks:andesite_pebble').withChance(0.5),
+        Item.of('infinite_horizons_tweaks:diorite_pebble').withChance(0.5),
+        Item.of('infinite_horizons_tweaks:granite_pebble').withChance(0.5)
     ], ['minecraft:gravel','createsifter:string_mesh']).id('kubejs:recipes/createsiftersifting/string/gravel');
     event.recipes.createsifterSifting([
         Item.of('minecraft:flint').withChance(0.25),
@@ -24,7 +24,7 @@ ServerEvents.recipes(event => {
         Item.of('minecraft:raw_copper').withChance(0.25),
         Item.of('minecraft:raw_iron').withChance(0.25),
         Item.of('infinite_resources:raw_aluminum').withChance(0.25),
-        Item.of('kubejs:raw_osmium').withChance(0.25),
+        Item.of('infinite_resources:raw_osmium').withChance(0.25),
         Item.of('infinite_resources:raw_tin').withChance(0.5),
         Item.of('infinite_resources:raw_zinc').withChance(0.5),
         Item.of('minecraft:lapis_lazuli').withChance(0.25),
@@ -35,7 +35,7 @@ ServerEvents.recipes(event => {
         Item.of('infinite_resources:raw_zinc').withChance(0.25),
         Item.of('infinite_resources:raw_aluminum').withChance(0.375),
         Item.of('infinite_resources:raw_tin').withChance(0.25),
-        Item.of('kubejs:raw_osmium').withChance(0.5),
+        Item.of('infinite_resources:raw_osmium').withChance(0.5),
         Item.of('minecraft:lapis_lazuli').withChance(0.5),
         Item.of('minecraft:coal').withChance(0.5),
         Item.of('minecraft:emerald').withChance(0.25),
