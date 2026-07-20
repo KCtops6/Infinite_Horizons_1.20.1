8434524aaac6f1a8c42dd548c3537a28fb61443e
KCtops6 <kctops06@gmail.com>
2026-07-19 20:44:06 -0400

    Fixed pebble tag and pebble to lava recipe.

--- kubejs/server_scripts/normal mode/server-events/Tags/item.js
+++ kubejs/server_scripts/normal mode/server-events/Tags/item.js
@@ -44,7 +44,11 @@ ServerEvents.tags('item', event => {
     forbidden_arcanus_scales.forEach(scale => {
         event.remove('forge:dragon_scales', scale);
     });
-    let stone_pebbles = ['twigs:pebble', 'botania:pebble', 'kubejs:stone_pebble'];
+    let stone_pebbles = [
+        'infinite_horizons_tweaks:stone_pebble', 'infinite_horizons_tweaks:andesite_pebble',
+        'infinite_horizons_tweaks:diorite_pebble', 'infinite_horizons_tweaks:granite_pebble',
+        'infinite_horizons_tweaks:deepslate_pebble', 'infinite_horizons_tweaks:tuff_pebble'
+    ];
     stone_pebbles.forEach(s => {
         event.add('kubejs:pebbles', s);
     });
