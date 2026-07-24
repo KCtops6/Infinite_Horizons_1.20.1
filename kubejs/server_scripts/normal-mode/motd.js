369e52ed32f8fd44cf82bbc112ad4171e1a3f3f6
KCtops6 <kctops06@gmail.com>
2026-07-24 18:29:49 -0400

    Fixed andesite/diorite/granite to gravel recipes

--- kubejs/server_scripts/normal-mode/motd.js
+++ kubejs/server_scripts/normal-mode/motd.js
@@ -1,12 +1,12 @@
 PlayerEvents.loggedIn(event => {
     event.player.tell(Text.of(`§6§lWelcome to §aInfinite Horizons§6!`));
-    event.player.tell(Text.darkRed('Note, the following mods will be removed in Version 14.1: ')
-        .append(Text.red('Amendments').bold())
-        .append(Text.red(', '))
-        .append(Text.red('Custom Villager Professions (Forge)').bold())
-        .append(Text.red(', '))
-        .append(Text.red('Environmental Materials').bold())
-        .append(Text.darkRed('. Feel free to readd these mods if you enjoy them. This is a decision about refining the pack.')));
+    // event.player.tell(Text.darkRed('Note, the following mods will be removed in Version 14.1: ')
+    //     .append(Text.red('Amendments').bold())
+    //     .append(Text.red(', '))
+    //     .append(Text.red('Custom Villager Professions (Forge)').bold())
+    //     .append(Text.red(', '))
+    //     .append(Text.red('Environmental Materials').bold())
+    //     .append(Text.darkRed('. Feel free to readd these mods if you enjoy them. This is a decision about refining the pack.')));
     event.player.tell(Text.darkRed('Note, the following old KubeJS/"Infinite Horizons" items will be depreciated in Version 14.1: ')
         .append(Text.red('Wooden/Stone/Iron Trowel').bold())
         .append(Text.red(', '))
--- kubejs/server_scripts/normal-mode/server-events/recipes/create/milling.js
+++ kubejs/server_scripts/normal-mode/server-events/recipes/create/milling.js
@@ -9,13 +9,13 @@ ServerEvents.recipes(event => {
     };
 
     var recipes = [
-        { input: 'minecraft:andesite', output: 'kubejs:andesite_gravel' },
-        { input: 'minecraft:diorite', output: 'kubejs:diorite_gravel' },
-        { input: 'minecraft:granite', output: 'kubejs:granite_gravel' },
+        { input: 'minecraft:andesite', output: 'infinite_horizons_tweaks:andesite_gravel' },
+        { input: 'minecraft:diorite', output: 'infinite_horizons_tweaks:diorite_gravel' },
+        { input: 'minecraft:granite', output: 'infinite_horizons_tweaks:granite_gravel' },
         { input: 'minecraft:gravel', output: 'minecraft:sand' },
-        { input: 'minecraft:tuff', output: 'kubejs:tuff_gravel' },
+        { input: 'minecraft:tuff', output: 'infinite_horizons_tweaks:tuff_gravel' },
         { input: 'minecraft:sand', output: 'createsifter:dust' },
-        { input: 'minecraft:cobbled_deepslate', output: 'kubejs:deepslate_gravel' },
+        { input: 'minecraft:cobbled_deepslate', output: 'infinite_horizons_tweaks:deepslate_gravel' },
         { input: 'minecraft:basalt', output: 'kubejs:basalt_gravel' },
         { input: 'create:limestone', output: 'kubejs:limestone_gravel' },
         { input: 'forbidden_arcanus:darkstone', output: 'kubejs:darkstone_gravel' },
