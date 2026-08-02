8e3a20afeb7c581f5a9fcdc61d59e60ce18a9381
KCtops6 <kctops06@gmail.com>
2026-08-02 16:05:26 -0400

    Player can obtain mycelium spores, red mushroom spores, and brown mushroom spores via Create sifting dirt Player can obtain crimson and warped mushroom spores by shearing nether/warped wart blocks respectfully Player can obtain mycelium block by right clicking a grass block in-world with mycelium spores Player can obtain brown/red mushrooms by right clicking respective spores on mycelium block Player can obtain warped/crimson nylium by right clicking respective spores on netherrack Player can obtain warped/crimson mushrooms by right clicking respective spores on respective nylium blocks

--- kubejs/server_scripts/normal-mode/block-events/right-clicked/mushroom-spores.js
+++ kubejs/server_scripts/normal-mode/block-events/right-clicked/mushroom-spores.js
@@ -1,45 +1,69 @@
 BlockEvents.rightClicked(event => {
     var { block, item, player, hand, level } = event;
-    if (hand !== 'MAIN_HAND' || item.id !== 'minecraft:shears') return;
-    var nyliumMap = {
-        'minecraft:nether_wart_block': 'kubejs:crimson_nylium_spores',
-        'minecraft:warped_wart_block': 'kubejs:warped_nylium_spores'
-    };
-    if (!level.isClientSide()) {
-        if (block.id in nyliumMap) {
-            player.give(nyliumMap[block.id]);
-            if (!player.isCreative()) item.setDamageValue(item.getDamageValue() + 1);
+    if (hand !== 'MAIN_HAND') return;
+    if (item.id === 'minecraft:shears') {
+        var nyliumMap = {
+            'minecraft:nether_wart_block': 'kubejs:crimson_nylium_spores',
+            'minecraft:warped_wart_block': 'kubejs:warped_nylium_spores'
+        };
+        if (!level.isClientSide()) {
+            if (block.id in nyliumMap) {
+                player.give(nyliumMap[block.id]);
+                if (!player.isCreative()) item.setDamageValue(item.getDamageValue() + 1);
+            } 
+            else if (block.id === 'minecraft:mycelium') {
+                var mushroomSpore = Math.random() < 0.5 
+                    ? 'kubejs:red_mushroom_spores' 
+                    : 'kubejs:brown_mushroom_spores';
+                player.give(mushroomSpore);
+                if (!player.isCreative()) item.setDamageValue(item.getDamageValue() + 1);
+            }
         }
-        else if (block.id === 'minecraft:mycelium') {
-            var mushroomSpore = Math.random() < 0.5 
-                ? 'kubejs:red_mushroom_spores' 
-                : 'kubejs:brown_mushroom_spores';
-            player.give(mushroomSpore);
-            if (!player.isCreative()) item.setDamageValue(item.getDamageValue() + 1);
-        }
+        return;
     }
-});
-
-BlockEvents.rightClicked(event => {
-    var sporeMap = {
-        'kubejs:crimson_nylium_spores': 'minecraft:crimson_fungus',
-        'kubejs:warped_nylium_spores': 'minecraft:warped_fungus',
+    if (item.id === 'kubejs:mycellium_spores' && block.id === 'minecraft:grass_block') {
+        if (!level.isClientSide()) {
+            block.set('minecraft:mycelium');
+            if (!player.isCreative()) item.count--;
+        }
+        return;
+    }
+    var mushroomMap = {
         'kubejs:red_mushroom_spores': 'minecraft:red_mushroom',
         'kubejs:brown_mushroom_spores': 'minecraft:brown_mushroom'
     };
-    var { block, item, player, hand, level } = event;
-    if (hand !== 'MAIN_HAND' || !(item.id in sporeMap)) return;
-    if (!level.isClientSide()) {
-        block.offset('up').set(sporeMap[item.id]);
-        if (!player.isCreative()) item.count--;
+    if (item.id in mushroomMap && block.id === 'minecraft:mycelium') {
+        if (!level.isClientSide()) {
+            block.offset('up').set(mushroomMap[item.id]);
+            if (!player.isCreative()) item.count--;
+        }
+        return;
     }
-});
-
-BlockEvents.rightClicked(event => {
-    var { block, item, player, hand, level } = event;
-    if (hand !== 'MAIN_HAND' && item.id === 'kubejs:mycellium_spores' && block.id === 'minecraft:grass_block') return;
-    if (!level.isClientSide()) {
-        block.set('minecraft:mycelium');
-        if (!player.isCreative()) item.count--;
+    var nyliumSporeData = {
+        'kubejs:crimson_nylium_spores': {
+            nyliumBlock: 'minecraft:crimson_nylium',
+            fungus: 'minecraft:crimson_fungus'
+        },
+        'kubejs:warped_nylium_spores': {
+            nyliumBlock: 'minecraft:warped_nylium',
+            fungus: 'minecraft:warped_fungus'
+        }
+    };
+    if (item.id in nyliumSporeData) {
+        var data = nyliumSporeData[item.id];
+        if (block.id === 'minecraft:netherrack') {
+            if (!level.isClientSide()) {
+                block.set(data.nyliumBlock);
+                if (!player.isCreative()) item.count--;
+            }
+            return;
+        }
+        if (block.id === data.nyliumBlock) {
+            if (!level.isClientSide()) {
+                block.offset('up').set(data.fungus);
+                if (!player.isCreative()) item.count--;
+            }
+            return;
+        }
     }
 });
