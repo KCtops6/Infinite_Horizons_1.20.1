9ed6bbae4c4f5e56dd386e5373c52708049d205c
KCtops6 <kctops06@gmail.com>
2026-08-03 16:34:49 -0400

    Fixed village weaponsmith giving old KubeJS ingots

--- kubejs/server_scripts/normal-mode/LootJS/Modifiers/replace-loot.js
+++ kubejs/server_scripts/normal-mode/LootJS/Modifiers/replace-loot.js
@@ -1,8 +1,12 @@
 // kubejs\server_scripts\replace-loot.js
 LootJS.modifiers(event => {
     event.addLootTableModifier('immersiveengineering:chests/engineers_house')
-        .replaceLoot('immersiveengineering:ingot_aluminum', 'infinite_horizons_tweaks:aluminum_ingot')
-        .replaceLoot('immersiveengineering:nugget_silver', 'infinite_horizons_tweaks:silver_nugget')
-        .replaceLoot('immersiveengineering:nugget_lead', 'infinite_horizons_tweaks:lead_nugget')
-        .replaceLoot('immersiveengineering:nugget_nickel', 'infinite_horizons_tweaks:nickel_nugget');
+        .replaceLoot('immersiveengineering:ingot_aluminum', 'infinite_resources:aluminum_ingot')
+        .replaceLoot('immersiveengineering:nugget_silver', 'infinite_resources:silver_nugget')
+        .replaceLoot('immersiveengineering:nugget_lead', 'infinite_resources:lead_nugget')
+        .replaceLoot('immersiveengineering:nugget_nickel', 'infinite_resources:nickel_nugget');
+    event.addLootTableModifier('minecraft:chests/village/village_weaponsmith')
+        .replaceLoot('kubejs:brass_ingot', 'infinite_resources:brass_ingot')
+        .replaceLoot('kubejs:steel_ingot', 'infinite_resources:steel_ingot')
+        .replaceLoot('kubejs:bronze_ingot', 'infinite_resources:bronze_ingot');
 }); 
