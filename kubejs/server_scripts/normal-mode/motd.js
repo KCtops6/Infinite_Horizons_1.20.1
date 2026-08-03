3d044d601a543ad68174d95ea769a3c7927a69d4
KCtops6 <kctops06@gmail.com>
2026-08-03 13:32:39 -0400

    temp dis motd

--- kubejs/server_scripts/normal-mode/motd.js
+++ kubejs/server_scripts/normal-mode/motd.js
@@ -6,11 +6,11 @@ PlayerEvents.loggedIn(event => {
     //     .append(Text.red('Custom Villager Professions (Forge)').bold())
     //     .append(Text.red(', '))
     //     .append(Text.red('Environmental Materials').bold())
-    //     .append(Text.darkRed('. Feel free to readd these mods if you enjoy them. This is a decision about refining the pack.')));
-    event.player.tell(Text.darkRed('Note, the following old KubeJS/"Infinite Horizons" items will be depreciated in Version 14.1: ')
-        .append(Text.red('Wooden/Stone/Iron Trowel').bold())
-        .append(Text.red(', '))
-        .append(Text.red('Stone/Andesite/Diorite/Granite/Deepslate Pebbles').bold())
-        .append(Text.red(', '))
-        .append(Text.red('Dungeon Keys').bold()));
+    // //     .append(Text.darkRed('. Feel free to readd these mods if you enjoy them. This is a decision about refining the pack.')));
+    // event.player.tell(Text.darkRed('Note, the following old KubeJS/"Infinite Horizons" items will be depreciated in Version 14.1: ')
+    //     .append(Text.red('Wooden/Stone/Iron Trowel').bold())
+    //     .append(Text.red(', '))
+    //     .append(Text.red('Stone/Andesite/Diorite/Granite/Deepslate Pebbles').bold())
+    //     .append(Text.red(', '))
+    //     .append(Text.red('Dungeon Keys').bold()));
 });
