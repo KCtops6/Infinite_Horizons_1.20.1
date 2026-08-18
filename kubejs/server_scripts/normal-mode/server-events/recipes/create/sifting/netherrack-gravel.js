2d5cf89592b26ef8265fb6f5d373854e8a6d140a
KCtops6 <kctops06@gmail.com>
2026-08-18 19:32:43 -0400

    Added Gregtech Certus Quartz and Nether Quartz veins to Create Sifting (Steel Mesh + Netherrack)

--- kubejs/server_scripts/normal-mode/server-events/recipes/create/sifting/netherrack-gravel.js
+++ kubejs/server_scripts/normal-mode/server-events/recipes/create/sifting/netherrack-gravel.js
@@ -10,4 +10,10 @@ ServerEvents.recipes(event => {
         Item.of('kubejs:raw_uranium').withChance(0.25),
         Item.of('occultism:raw_iesnium').withChance(0.0625)
     ], ['kubejs:netherrack_gravel', 'kubejs:netherite_mesh']).id('kubejs:recipes/createsiftersifting/netherite/netherrack');
-})
+    event.recipes.createsifterSifting([
+        Item.of('gtceu:raw_nether_quartz').withChance(0.25),
+        Item.of('gtceu:raw_quartzite').withChance(0.2083),
+        Item.of('gtceu:raw_certus_quartz').withChance(0.083),
+        Item.of('gtceu:raw_barite').withChance(0.04167)
+    ], ['kubejs:netherrack_gravel', 'kubejs:steel_mesh']).id('kubejs:recipes/createsiftersifting/steel/netherrack');
+});
