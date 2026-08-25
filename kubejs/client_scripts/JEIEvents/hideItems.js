573547dc91fc4dbd163f63138e92e4b97cee9506
KCtops6 <kctops06@gmail.com>
2026-08-25 18:46:49 -0400

    Instead of hiding from JEI, many items were just removed from the game

--- kubejs/client_scripts/JEIEvents/hideItems.js
+++ kubejs/client_scripts/JEIEvents/hideItems.js
@@ -1,18 +1,5 @@
 JEIEvents.hideItems(event => {
     const modsToHide = {
-        'extendedcrafting': [
-            'luminessence_block', 'redstone_ingot_block', 'enhanced_redstone_ingot_block',
-            'ender_ingot_block', 'enhanced_ender_ingot_block', 'crystaltine_block',
-            'the_ultimate_block', 'nether_star_block', 'flux_star_block',
-            'ender_star_block', 'black_iron_nugget', 'redstone_nugget',
-            'enhanced_redstone_nugget', 'ender_nugget', 'enhanced_ender_nugget',
-            'crystaltine_nugget', 'the_ultimate_nugget', 'black_iron_block',
-            'black_iron_ingot', 'black_iron_slate', 'the_ultimate_ingot',
-            'the_ultimate_nugget', 'the_ultimate_catalyst', 'the_ultimate_component'
-        ],
-        'createsifter': [
-            'andesite_mesh', 'zinc_mesh', 'custom_mesh', 'advanced_brass_mesh', 'advanced_custom_mesh'
-        ],
         'ae2': ['facade'],
         'enderio': ['filled_soul_vial', 'broken_spawner'],
         'mekanism': ['digital_miner', 'creative_fluid_tank'],
